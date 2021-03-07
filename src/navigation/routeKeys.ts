539784c78bcfbd96e9bf704d3d38592704c80e41
Jose Morales <jose.morales.github@gmail.com>
2021-03-07 18:45:29 +0100

    adding new navigation module

---
+++ src/navigation/routeKeys.ts
@@ -0,0 +1,8 @@
+export const dashboard = 'Dashboard' as const;
+export const tips = 'Tips' as const;
+export const tipDetail = 'TipDetail' as const;
+export const registrarList = 'RegistrarList' as const;
+export const motionDetail = 'MotionDetail' as const;
+
+export const webview = 'Webview' as const;
+export const devScreen = 'DevScreen' as const;
