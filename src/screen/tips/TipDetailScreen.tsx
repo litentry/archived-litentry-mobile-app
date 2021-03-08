044f03bdf2d937093c9f70a3b8c16b0efb62707d
Jose Morales <jose.morales.github@gmail.com>
2021-03-08 20:10:09 +0100

    testing navigation to the detail screen

--- src/screen/tips/TipDetailScreen.tsx
+++ src/screen/tips/TipDetailScreen.tsx
@@ -9,12 +9,13 @@ type ScreenProps = {
   route: RouteProp<DashboardStackParamList, 'TipDetail'>;
 };
 
-function TipDetailScreen({navigation}: ScreenProps) {
+function TipDetailScreen({navigation, route}: ScreenProps) {
   return (
     <GenericNavigationLayout
       title="Tip"
       onBackPressed={() => navigation.goBack()}>
       <Text>Tip Detail Screen</Text>
+      <Text>{route.params?.hash}</Text>
     </GenericNavigationLayout>
   );
 }
