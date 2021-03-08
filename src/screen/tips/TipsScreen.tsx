699f6b6ed2f9ca06a031ebc01f95b2840ef9defe
Jose Morales <jose.morales.github@gmail.com>
2021-03-08 20:09:23 +0100

    adding Tips list to the screen

--- src/screen/tips/TipsScreen.tsx
+++ src/screen/tips/TipsScreen.tsx
@@ -1,20 +1,38 @@
 import React from 'react';
-import {Text} from '@ui-kitten/components';
-import {DrawerNavigationProp} from '@react-navigation/drawer';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {StyleSheet, View} from 'react-native';
+import {List, Divider} from '@ui-kitten/components';
 import {RouteProp} from '@react-navigation/native';
+
 import GenericNavigationLayout from 'presentational/GenericNavigationLayout';
+import {useTips} from 'src/hook/useTips';
+import TipTeaser from 'layout/TipTeaser';
 
 type ScreenProps = {
-  navigation: DrawerNavigationProp<DrawerParamList>;
+  navigation: StackNavigationProp<DashboardStackParamList>;
   route: RouteProp<DashboardStackParamList, 'TipDetail'>;
 };
 
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+  },
+});
+
 function TipsScreen({navigation}: ScreenProps) {
+  const tips = useTips();
+
   return (
     <GenericNavigationLayout
       title="Tips"
       onBackPressed={() => navigation.goBack()}>
-      <Text>Tips Screen</Text>
+      <View style={styles.container}>
+        <List
+          data={tips}
+          ItemSeparatorComponent={Divider}
+          renderItem={({item}) => <TipTeaser tip={item} />}
+        />
+      </View>
     </GenericNavigationLayout>
   );
 }
