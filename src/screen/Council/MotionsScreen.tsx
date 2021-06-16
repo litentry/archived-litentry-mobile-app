9503cb9a7dcd0655c83402db160425082847b581
Amir Hoseinian <hoseinian.amir@gmail.com>
2021-06-16 09:11:40 +0200

    Add motions screen
    fixes #127

--- src/screen/Council/MotionsScreen.tsx
+++ src/screen/Council/MotionsScreen.tsx
@@ -1,6 +1,38 @@
-import {Text} from '@ui-kitten/components';
-import React from 'react';
+import {Icon, Layout, Text, TopNavigationAction} from '@ui-kitten/components';
+import React, {useContext} from 'react';
+import globalStyles from 'src/styles';
+import ScreenNavigation from 'layout/ScreenNavigation';
+import {NavigationProp} from '@react-navigation/native';
+import {ChainApiContext} from 'context/ChainApiContext';
+import useAsyncRetry from 'react-use/lib/useAsyncRetry';
 
-export function MotionsScreen() {
-  return <Text>Motions</Text>;
+export function MotionsScreen({navigation}: {navigation: NavigationProp<DashboardStackParamList>}) {
+  const {api} = useContext(ChainApiContext);
+
+  const {loading, value: data} = useAsyncRetry(async () => {
+    try {
+      if (api) {
+        const proposals = await api.derive.council.proposals();
+        return proposals;
+      }
+    } catch (e) {
+      console.warn(e);
+    }
+  }, [api]);
+
+  return (
+    <Layout style={globalStyles.flex}>
+      <ScreenNavigation
+        renderTitle={() => (
+          <Text category={'s1'} style={globalStyles.monoFont}>
+            Motions
+          </Text>
+        )}
+        accessoryLeft={
+          <TopNavigationAction onPress={navigation.goBack} icon={(p) => <Icon {...p} name={'arrow-back-outline'} />} />
+        }
+      />
+      <Text>Motions</Text>
+    </Layout>
+  );
 }
