3ba817ce4e6f8abe796e502c8cab55d9d8cd37af
Amir Hoseinian <hoseinian.amir@gmail.com>
2021-06-17 09:30:44 +0200

    Clean up and add Aye/Nay buttons
    fixes #127

--- src/screen/Council/MotionsScreen.tsx
+++ src/screen/Council/MotionsScreen.tsx
@@ -1,23 +1,29 @@
-import {Button, Icon, Layout, Text, TopNavigationAction} from '@ui-kitten/components';
-import React, {useContext, useMemo} from 'react';
+import {Button, Divider, Icon, Layout, Text, TopNavigationAction, useTheme} from '@ui-kitten/components';
+import React, {useContext, useMemo, useState} from 'react';
 import globalStyles, {standardPadding} from 'src/styles';
 import ScreenNavigation from 'layout/ScreenNavigation';
 import {NavigationProp} from '@react-navigation/native';
 import {ChainApiContext} from 'context/ChainApiContext';
 import useAsyncRetry from 'react-use/lib/useAsyncRetry';
-import {FlatList, StyleSheet, View} from 'react-native';
-import {Call, FunctionMetadataLatest} from '@polkadot/types/interfaces';
+import {FlatList, StyleSheet, TouchableOpacity, View} from 'react-native';
+import {FunctionMetadataLatest} from '@polkadot/types/interfaces';
 import {formatNumber} from '@polkadot/util';
 import {AccountContext} from 'context/AccountContextProvider';
 import {useQuery} from 'react-query';
 import {useVotingStatus} from '../../hook/useVotingStatus';
 import type {DeriveCollectiveProposal} from '@polkadot/api-derive/types';
 import {TxContext} from 'context/TxContext';
+import {EmptyView} from 'presentational/EmptyView';
+import Padder from 'presentational/Padder';
 
 export function MotionsScreen({navigation}: {navigation: NavigationProp<DashboardStackParamList>}) {
   const {api} = useContext(ChainApiContext);
 
-  const {value: data} = useAsyncRetry(async () => {
+  const {
+    value: data,
+    retry,
+    loading,
+  } = useAsyncRetry(async () => {
     try {
       if (api) {
         return api.derive.council.proposals();
@@ -42,12 +48,16 @@ export function MotionsScreen({navigation}: {navigation: NavigationProp<Dashboar
         }
       />
       <FlatList
+        refreshing={loading}
+        onRefresh={retry}
         style={styles.flatList}
         data={data}
         renderItem={({item}) => {
           return <Motion item={item} members={members} />;
         }}
+        ItemSeparatorComponent={Divider}
         keyExtractor={(item) => item.hash.toHex()}
+        ListEmptyComponent={EmptyView}
       />
     </Layout>
   );
@@ -56,93 +66,120 @@ export function MotionsScreen({navigation}: {navigation: NavigationProp<Dashboar
 const styles = StyleSheet.create({flatList: {padding: standardPadding * 2}});
 
 function Motion({item, members}: {item: DeriveCollectiveProposal; members: string[]}) {
-  const {isCloseable} = useVotingStatus(item.votes, members.length, 'council');
+  const theme = useTheme();
   const {api} = useContext(ChainApiContext);
   const {start} = useContext(TxContext);
   const {accounts} = useContext(AccountContext);
   const account = accounts?.[0];
 
+  const {votes, proposal, hash} = item;
+  const {isCloseable, isVoteable} = useVotingStatus(votes, members.length, 'council');
+
+  const {meta, method, section} = proposal.registry.findMetaCall(proposal.callIndex);
+
+  const [open, setOpen] = useState(false);
+
   return (
     <View style={motionStyle.container}>
-      <Text category={'h4'}>{formatNumber(item.votes?.index)}</Text>
-      <CallExpander value={item.proposal} />
-      {/*<Text>{formatNumber(item.votes?.threshold)}</Text>*/}
-      <Text>{` ${item.votes?.ayes.length}/${members.length} `}</Text>
-      {isCloseable && (
-        <Button
-          status={'warning'}
-          size={'tiny'}
-          onPress={() => {
-            if (api && account) {
-              start({
-                api,
-                address: account.address,
-                title: 'treasury.approveProposal',
-                description:
-                  'Approve a proposal. At a later time, the proposal will be allocated to the beneficiary and the original deposit will be returned.',
-                params:
-                  api.tx.council.close?.meta.args.length === 4
-                    ? [item.hash, item.votes?.index, 0, 0]
-                    : [item.hash, item.votes?.index],
-                txMethod: 'council.close',
-              })
-                .then(() => console.log('success'))
-                .catch((e) => console.warn(e));
-            }
-          }}>
-          Close
-        </Button>
-      )}
+      <View style={motionStyle.mainRow}>
+        <Text category={'h4'}>{formatNumber(votes?.index)}</Text>
+        <Text category={'p1'} style={motionStyle.title} numberOfLines={1}>
+          {section}.{method}
+        </Text>
+        {/*<Text>{formatNumber(votes?.threshold)}</Text>*/}
+        <TouchableOpacity onPress={() => setOpen(!open)}>
+          <Icon
+            name={open ? 'arrow-up-outline' : 'arrow-down-outline'}
+            style={globalStyles.icon}
+            fill={theme['color-basic-600']}
+          />
+        </TouchableOpacity>
+        <Text>{` ${votes?.ayes.length}/${members.length} `}</Text>
+        <Padder scale={0.5} />
+        {isCloseable ? (
+          <View>
+            <Button
+              status={'warning'}
+              size={'tiny'}
+              onPress={() => {
+                if (api && account) {
+                  start({
+                    api,
+                    address: account.address,
+                    title: 'treasury.approveProposal',
+                    description:
+                      'Approve a proposal. At a later time, the proposal will be allocated to the beneficiary and the original deposit will be returned.',
+                    params:
+                      api.tx.council.close?.meta.args.length === 4 ? [hash, votes?.index, 0, 0] : [hash, votes?.index],
+                    txMethod: 'council.close',
+                  })
+                    .then(() => console.log('success'))
+                    .catch((e) => console.warn(e));
+                }
+              }}>
+              Close
+            </Button>
+          </View>
+        ) : isVoteable ? (
+          <View style={motionStyle.buttons}>
+            <Button
+              status={'danger'}
+              size={'tiny'}
+              onPress={() => {
+                if (api && account) {
+                  start({
+                    api,
+                    address: account.address,
+                    title: 'council.vote(proposal, index, approve)',
+                    description: 'Add a nay vote for the sender to the given proposal.',
+                    params: [hash, votes?.index, false],
+                    txMethod: 'council.vote',
+                  })
+                    .then(() => console.log('success'))
+                    .catch((e) => console.warn(e));
+                }
+              }}>
+              Nay
+            </Button>
+            <Padder scale={0.5} />
+            <Button
+              status={'success'}
+              size={'tiny'}
+              onPress={() => {
+                if (api && account) {
+                  start({
+                    api,
+                    address: account.address,
+                    title: 'council.vote(proposal, index, approve)',
+                    description: 'Add an aye vote for the sender to the given proposal.',
+                    params: [hash, votes?.index, true],
+                    txMethod: 'council.vote',
+                  })
+                    .then(() => console.log('success'))
+                    .catch((e) => console.warn(e));
+                }
+              }}>
+              Aye
+            </Button>
+          </View>
+        ) : null}
+      </View>
+      {open ? (
+        <Text category={'c1'} style={[motionStyle.desc, {color: theme['color-basic-600']}]}>{`${formatCallMeta(
+          meta,
+        )}`}</Text>
+      ) : null}
     </View>
   );
 }
 
-const motionStyle = StyleSheet.create({container: {flexDirection: 'row', alignItems: 'center'}});
-
-function CallExpander({
-  value,
-}: {
-  children?: React.ReactNode;
-  className?: string;
-  labelHash?: React.ReactNode;
-  value: Call;
-  withHash?: boolean;
-}) {
-  const {meta, method, section} = value.registry.findMetaCall(value.callIndex);
-  return (
-    <View style={{flex: 1}}>
-      <Text category={'c1'}>
-        {section}.{method}
-      </Text>
-      <Text category={'c1'} numberOfLines={1}>{`${formatMeta(meta)}`}</Text>
-    </View>
-  );
-}
-
-function splitSingle(value: string[], sep: string): string[] {
-  return value.reduce((result: string[], value: string): string[] => {
-    return value.split(sep).reduce((result: string[], value: string) => result.concat(value), result);
-  }, []);
-}
-
-function splitParts(value: string): string[] {
-  return ['[', ']'].reduce((result: string[], sep) => splitSingle(result, sep), [value]);
-}
-
-function formatMeta(meta?: FunctionMetadataLatest): string {
-  if (!meta || !meta.documentation.length) {
-    return '';
-  }
-
-  const strings = meta.documentation.map((doc) => doc.toString().trim());
-  const firstEmpty = strings.findIndex((doc) => !doc.length);
-  const combined = (firstEmpty === -1 ? strings : strings.slice(0, firstEmpty))
-    .join(' ')
-    .replace(/#(<weight>| <weight>).*<\/weight>/, '');
-  const parts = splitParts(combined.replace(/\\/g, '').replace(/`/g, ''));
-
-  return parts.join(' ');
-}
+const motionStyle = StyleSheet.create({
+  container: {paddingVertical: standardPadding},
+  mainRow: {flexDirection: 'row', alignItems: 'center'},
+  title: {padding: standardPadding, flex: 1},
+  desc: {paddingHorizontal: standardPadding},
+  buttons: {display: 'flex', flexDirection: 'row'},
+});
 
 interface Result {
   isMember: boolean;
@@ -161,4 +198,29 @@ export function useMembers(collective: 'council' | 'technicalCommittee' = 'counc
       members,
     };
   }, [accounts, retrieved]);
+}
+
+export function formatCallMeta(meta?: FunctionMetadataLatest): string {
+  if (!meta || !meta.documentation.length) {
+    return '';
+  }
+
+  const strings = meta.documentation.map((doc) => doc.toString().trim());
+  const firstEmpty = strings.findIndex((doc) => !doc.length);
+  const combined = (firstEmpty === -1 ? strings : strings.slice(0, firstEmpty))
+    .join(' ')
+    .replace(/#(<weight>| <weight>).*<\/weight>/, '');
+  const parts = splitParts(combined.replace(/\\/g, '').replace(/`/g, ''));
+
+  return parts.join(' ');
+}
+
+function splitSingle(value: string[], sep: string): string[] {
+  return value.reduce((result: string[], value: string): string[] => {
+    return value.split(sep).reduce((result: string[], value: string) => result.concat(value), result);
+  }, []);
+}
+
+function splitParts(value: string): string[] {
+  return ['[', ']'].reduce((result: string[], sep) => splitSingle(result, sep), [value]);
 }
