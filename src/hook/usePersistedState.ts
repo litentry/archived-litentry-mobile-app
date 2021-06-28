4628b7f197884f9a5436e657a574cb2949672213
Francisco Silva <franjs.francisco@gmail.com>
2021-06-28 10:46:06 +0200

    refactoring usePersistedDate

---
+++ src/hook/usePersistedState.ts
@@ -0,0 +1,39 @@
+import {useState, useEffect, useCallback} from 'react';
+import {setItem, getItem} from 'src/service/AsyncStorage';
+import {AsyncStorageKeyType} from 'src/types';
+import {createLogger} from 'src/utils';
+
+const logger = createLogger('usePersistedState');
+
+type UsePersistedStateResult<T> = [state: T | undefined, setState: (data: T) => void];
+
+export function usePersistedState<T>(key: AsyncStorageKeyType, initialState?: T): UsePersistedStateResult<T> {
+  const [state, setState] = useState(initialState);
+  const persistState = useCallback(
+    (newState: T) => {
+      try {
+        setState(newState);
+        setItem(key, newState);
+      } catch (e) {
+        logger.error(`Error setting ${key} to AsyncStorage`, e);
+      }
+    },
+    [key],
+  );
+
+  useEffect(() => {
+    const getPersistedState = async () => {
+      try {
+        const persistedState = await getItem<T>(key);
+        if (persistedState) {
+          setState(persistedState);
+        }
+      } catch (e) {
+        logger.error(`Error getting ${key} from AsyncStorage`, e);
+      }
+    };
+    getPersistedState();
+  }, [key]);
+
+  return [state, persistState];
+}
