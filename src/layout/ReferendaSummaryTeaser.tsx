1e437af74f79d5931c24280e4b3d7e0e0fdd2f9b
Amir Hoseinian <hoseinian.amir@gmail.com>
2021-07-13 11:51:42 +0200

    Fix typo

--- src/layout/ReferendaSummaryTeaser.tsx
+++ src/layout/ReferendaSummaryTeaser.tsx
@@ -1,4 +1,4 @@
-import SeactionTeaserContainer from 'presentational/SectionTeaserContainer';
+import SectionTeaserContainer from 'presentational/SectionTeaserContainer';
 import React from 'react';
 import {View} from 'react-native';
 
@@ -8,8 +8,8 @@ type Props = {
 
 export function ReferendaSummaryTeaser(props: Props) {
   return (
-    <SeactionTeaserContainer onMorePress={props.onMorePress} title="Refernda">
+    <SectionTeaserContainer onMorePress={props.onMorePress} title="Refernda">
       <View />
-    </SeactionTeaserContainer>
+    </SectionTeaserContainer>
   );
 }
