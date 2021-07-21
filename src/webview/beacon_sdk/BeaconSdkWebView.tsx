486b3660da252058779304760bda71ae44863eac
Amir Hoseinian <hoseinian.amir@gmail.com>
2021-07-21 15:15:16 +0200

    add beacon sdk

--- src/webview/beacon_sdk/BeaconSdkWebView.tsx
+++ src/webview/beacon_sdk/BeaconSdkWebView.tsx
@@ -2,9 +2,26 @@
 
 import React, {useState} from 'react';
 import {webViewRender, emit, useSubscribe} from 'react-native-react-bridge/lib/web';
+import {DAppClient} from '@airgap/beacon-sdk';
+import {useEffect} from 'react';
+
+const dAppClient = new DAppClient({name: 'Beacon Docs'});
 
 const Root = () => {
   const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      // try {
+      //   console.log('Requesting permissions...');
+      //   const permissions = await dAppClient.requestPermissions();
+      //   console.log('Got permissions:', permissions.address);
+      // } catch (error) {
+      //   console.log('Got error:', error);
+      // }
+    })();
+  }, []);
+
   // useSubscribe hook receives message from React Native
   useSubscribe((message: any) => {
     if (message.type === 'success') {
