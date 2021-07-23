c671e1b79bdd78ecb1d5cec612519cd743c8bbcd
Amir Hoseinian <hoseinian.amir@gmail.com>
2021-07-23 14:59:15 +0200

    Handle beacon load requests with RN

--- src/screen/BeaconWebViewScreen.tsx
+++ src/screen/BeaconWebViewScreen.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import SafeView, {noTopEdges} from 'presentational/SafeView';
 import WebView from 'react-native-webview';
+import {Linking} from 'react-native';
 
 export function BeaconWebViewScreen() {
   return (
     <SafeView edges={noTopEdges}>
-      <WebView style={{flex: 1}} source={require('../beacon/index_inline.html')} />
+      <WebView
+        style={{flex: 1}}
+        source={require('src/beacon/index_inline.html')}
+        onShouldStartLoadWithRequest={(request) => {
+          if (!request.url.includes('index_inline')) {
+            Linking.openURL(request.url);
+            return false;
+          }
+
+          return true;
+        }}
+      />
     </SafeView>
   );
 }
