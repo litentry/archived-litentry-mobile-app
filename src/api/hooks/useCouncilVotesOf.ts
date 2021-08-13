949a8a097f671cc81a3fb2d51f905b16bdab4282
Francisco Silva <franjs.francisco@gmail.com>
2021-08-13 13:18:51 +0200

    adding new hook to get the votes of an account

---
+++ src/api/hooks/useCouncilVotesOf.ts
@@ -0,0 +1,9 @@
+import {ApiPromise} from '@polkadot/api';
+import type {AccountId} from '@polkadot/types/interfaces';
+import useApiQuery from 'src/api/hooks/useApiQuery';
+
+export function useCouncilVotesOf(accountId: string | Uint8Array | AccountId) {
+  return useApiQuery(['council-votes', {accountId}], async (api: ApiPromise) => {
+    return await api.derive.council.votesOf(accountId);
+  });
+}
