6ba87f664300ac9b6b7bbebafac109669001663e
Francisco Silva <franjs.francisco@gmail.com>
2021-08-20 17:05:00 +0200

    removing useTX

--- src/context/TxContext/index.tsx
+++ src/context/TxContext/index.tsx
@@ -10,7 +10,7 @@ import ErrorDialog from 'presentational/ErrorDialog';
 import LoadingView from 'presentational/LoadingView';
 import SuccessDialog from 'presentational/SuccessDialog';
 import TxPayloadQr from 'presentational/TxPayloadQr';
-import React, {createContext, useCallback, useContext, useMemo, useReducer, useRef} from 'react';
+import React, {createContext, useCallback, useMemo, useReducer, useRef} from 'react';
 import {Dimensions, StyleSheet} from 'react-native';
 import {Modalize} from 'react-native-modalize';
 import QRCodeScanner from 'react-native-qrcode-scanner';
@@ -30,8 +30,6 @@ type TxContextValueType = {
 export const TxContext = createContext<TxContextValueType>({
   start: () => Promise.resolve(),
 });
-
-export const useTX = () => useContext(TxContext);
 
 const AlertIcon = (props: IconProps) => <Icon fill="#ccc" {...props} name="alert-triangle-outline" />;
 
