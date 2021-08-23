abec46ec5026f61729ce59c33a9ddefb7fce7880
Francisco Silva <franjs.francisco@gmail.com>
2021-08-23 10:26:54 +0200

    make tx config more flexible

--- src/api/hooks/useApiTx.ts
+++ src/api/hooks/useApiTx.ts
@@ -1,6 +1,7 @@
 import {useContext} from 'react';
 import {useApi} from 'context/ChainApiContext';
 import {TxContext, StartConfig} from 'context/TxContext';
+import {ApiPromise} from '@polkadot/api';
 
 type TxConfig = Omit<StartConfig, 'api'>;
 
@@ -8,10 +9,13 @@ export function useApiTx() {
   const {start} = useContext(TxContext);
   const {api} = useApi();
 
-  return (config: TxConfig) => {
+  return (config: TxConfig | ((apiPromise: ApiPromise) => TxConfig)) => {
     if (!api) {
       return Promise.reject(new Error('API is not defined'));
     }
-    return start({api, ...config});
+
+    const startConfig = typeof config === 'function' ? config(api) : config;
+
+    return start({api, ...startConfig});
   };
 }
