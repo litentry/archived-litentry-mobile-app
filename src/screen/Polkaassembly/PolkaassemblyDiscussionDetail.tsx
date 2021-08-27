e23f958ea830bf78b6925dd5e24faceb8710cacc
Amir Hoseinian <hoseinian.amir@gmail.com>
2021-08-27 14:20:50 +0200

    Add content and comments count

--- src/screen/Polkaassembly/PolkaassemblyDiscussionDetail.tsx
+++ src/screen/Polkaassembly/PolkaassemblyDiscussionDetail.tsx
@@ -1,12 +1,16 @@
 import {RouteProp} from '@react-navigation/native';
 import {Text} from '@ui-kitten/components';
+import moment from 'moment';
+import Icon from 'presentational/Icon';
+import {Label} from 'presentational/Label';
 import LoadingView from 'presentational/LoadingView';
+import Padder from 'presentational/Padder';
 import SafeView, {noTopEdges} from 'presentational/SafeView';
 import React from 'react';
 import {ScrollView, StyleSheet, View} from 'react-native';
 import {usePolkaassemblyDiscussionDetail} from 'src/api/hooks/usePolkaassemblyDiscussionDetail';
 import {PolkaassemblyDiscussionStackParamList} from 'src/navigation/navigation';
-import {standardPadding} from 'src/styles';
+import globalStyles, {standardPadding} from 'src/styles';
 
 export function PolkaassemblyDiscussionDetail({
   route,
@@ -25,8 +29,27 @@ export function PolkaassemblyDiscussionDetail({
       <ScrollView style={styles.container}>
         <Text category="h6">{data.title ?? ''}</Text>
         <View style={styles.postDetailRow}>
-          <Text category="c2">{data.author?.username ?? ''}</Text>
-          <Text category="c1">posted in</Text>
+          <Text category="c2">{data.author?.username ?? ''} </Text>
+          <Text category="c1">posted in </Text>
+          <Label text={data.topic.name} />
+          <Text category="c1"> {moment(data.created_at).fromNow()}</Text>
+        </View>
+        <View style={styles.content}>
+          <Text style={styles.contentText}>{data.content ?? ''}</Text>
+        </View>
+        <View>
+          {data.comments.length ? (
+            <>
+              <View style={globalStyles.rowAlignCenter}>
+                <Icon name="message-circle-outline" style={globalStyles.icon15} fill="#ccc" animation="pulse" />
+                <Padder scale={0.3} />
+                <Text category="label" appearance="hint">
+                  {data.comments.length} comments
+                </Text>
+              </View>
+              <Padder scale={1} />
+            </>
+          ) : null}
         </View>
       </ScrollView>
     </SafeView>
@@ -42,5 +65,12 @@ const styles = StyleSheet.create({
     marginTop: standardPadding,
     flexDirection: 'row',
     alignItems: 'center',
+  },
+  content: {
+    marginTop: standardPadding * 4,
+    marginBottom: standardPadding * 2,
+  },
+  contentText: {
+    opacity: 0.7,
   },
 });
