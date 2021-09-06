00529291b46955b0c1fa26044f52271d3ebc444d
Amir Hoseinian <hoseinian.amir@gmail.com>
2021-09-06 12:47:49 +0200

    Use account info teaser for sub identities
    - make sure to reload the sub identities after save operation
    
    #352

--- src/api/hooks/useSubIdentities.ts
+++ src/api/hooks/useSubIdentities.ts
@@ -1,16 +1,7 @@
 import {ApiPromise} from '@polkadot/api';
-import type {AccountId} from '@polkadot/types/interfaces';
-import type {Data, Option} from '@polkadot/types';
-import type {ITuple} from '@polkadot/types/types';
-import {u8aToString} from '@polkadot/util';
-
 import useApiQuery from 'src/api/hooks/useApiQuery';
 import {useSubAccounts} from 'src/api/hooks/useSubAccounts';
-
-export type SubIdentity = {
-  accountId: string;
-  name: string;
-};
+import {getAccountIdentityInfo, IdentityInfo} from 'src/api/queryFunctions/getAccountIdentityInfo';
 
 const ACCOUNTS_INDEX = 1;
 
@@ -20,26 +11,8 @@ export function useSubIdentities(address?: string) {
 
   return useApiQuery(
     ['sub-identities', {address, subsCount: subAccounts?.length || 0}],
-    async (api: ApiPromise): Promise<Array<SubIdentity>> => {
-      const superAccountsOpt = await api.query.identity.superOf.multi<Option<ITuple<[AccountId, Data]>>>(subIds);
-      const superAccounts = superAccountsOpt.reduce(
-        (
-          subIdentities: Array<{accountId: string; name: string}>,
-          accountOpt: Option<ITuple<[AccountId, Data]>>,
-          index: number,
-        ) => {
-          const accountId = subIds[index];
-          if (accountId && accountOpt.isSome) {
-            const [, data] = accountOpt.unwrap();
-            if (data.isRaw) {
-              subIdentities.push({accountId, name: u8aToString(data.asRaw)});
-            }
-          }
-          return subIdentities;
-        },
-        [],
-      );
-      return superAccounts;
+    async (api: ApiPromise): Promise<Array<IdentityInfo>> => {
+      return await Promise.all(subIds.map((subId) => getAccountIdentityInfo(api, subId)));
     },
     {enabled: Boolean(address) && Boolean(subAccounts)},
   );
