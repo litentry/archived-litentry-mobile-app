ebfa10135864de32bca18e4932b669129da8b654
Amir Hoseinian <hoseinian.amir@gmail.com>
2021-09-20 17:42:58 +0200

    Fix icon names
    #355

--- src/screen/MyAccountScreen.tsx
+++ src/screen/MyAccountScreen.tsx
@@ -7,6 +7,7 @@ import Padder from 'presentational/Padder';
 import SafeView, {noTopEdges} from 'presentational/SafeView';
 import React from 'react';
 import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 import {useAccountIdentityInfo} from 'src/api/hooks/useAccountIdentityInfo';
 import {useBalance} from 'src/api/hooks/useBalance';
 import {useFormatBalance} from 'src/api/hooks/useFormatBalance';
@@ -31,7 +32,7 @@ export function MyAccountScreen({
 
   return (
     <SafeView edges={noTopEdges}>
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
         <View style={[styles.box, {backgroundColor: theme['background-basic-color-2']}]}>
           <Row title="Display Name" value={data?.display} />
           <Padder scale={1} />
@@ -51,13 +52,14 @@ export function MyAccountScreen({
             }
           />
         </View>
+        <Padder scale={1} />
         <TouchableOpacity
           style={styles.button}
           onPress={() => {
             navigation.navigate(balanceScreen, {address});
           }}>
-          <View style={[styles.iconContainer, {backgroundColor: theme['color-basic-500']}]}>
-            <Icon style={styles.icon} fill={theme['text-basic-color']} name="settings-2" />
+          <View style={[styles.iconContainer, {backgroundColor: theme['background-basic-color-2']}]}>
+            <Icon style={styles.icon} fill={theme['text-basic-color']} name="credit-card" />
           </View>
           <Padder scale={2} />
           <Text category="h6">Show Balance details</Text>
@@ -68,7 +70,7 @@ export function MyAccountScreen({
             navigation.navigate(myIdentityScreen, {address});
             navigation.navigate(identityGuideScreen);
           }}>
-          <View style={[styles.iconContainer, {backgroundColor: theme['color-basic-500']}]}>
+          <View style={[styles.iconContainer, {backgroundColor: theme['background-basic-color-2']}]}>
             <Icon style={styles.icon} fill={theme['text-basic-color']} name="settings-2" />
           </View>
           <Padder scale={2} />
@@ -80,13 +82,13 @@ export function MyAccountScreen({
             removeAccount(address);
             navigation.navigate(accountsScreen);
           }}>
-          <View style={[styles.iconContainer, {backgroundColor: theme['color-basic-500']}]}>
-            <Icon style={styles.icon} fill={theme['text-basic-color']} name="settings-2" />
+          <View style={[styles.iconContainer, {backgroundColor: theme['background-basic-color-2']}]}>
+            <Icon style={styles.icon} fill={theme['text-basic-color']} name="close-circle" />
           </View>
           <Padder scale={2} />
           <Text category="h6">Remove Account</Text>
         </TouchableOpacity>
-      </View>
+      </ScrollView>
     </SafeView>
   );
 }
@@ -117,13 +119,12 @@ const styles = StyleSheet.create({
   button: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
     padding: 10,
     margin: 10,
   },
   iconContainer: {
     borderRadius: 100,
-    padding: 10,
+    padding: 15,
   },
   icon: {
     width: 40,
