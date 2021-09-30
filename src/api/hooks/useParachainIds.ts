02940b5c929a41a8b223abd7efd662eb4bcd3219
Francisco Silva <franjs.francisco@gmail.com>
2021-09-30 15:23:53 +0200

    better cache name

--- src/api/hooks/useParachainIds.ts
+++ src/api/hooks/useParachainIds.ts
@@ -3,5 +3,5 @@ import type {ParaId} from '@polkadot/types/interfaces';
 import useApiQuery from 'src/api/hooks/useApiQuery';
 
 export function useParachainIds() {
-  return useApiQuery('parachains', async (api: ApiPromise) => api.query.paras?.parachains?.<ParaId[]>());
+  return useApiQuery('parachain_ids', async (api: ApiPromise) => api.query.paras?.parachains?.<ParaId[]>());
 }
