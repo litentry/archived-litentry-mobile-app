3b0eef81c511e3e02f6cc5f5d18c49f5a03b1824
Francisco Silva <franjs.francisco@gmail.com>
2021-09-30 14:53:27 +0200

    adding useParachainLeases hook

---
+++ src/api/hooks/useParachainLeases.ts
@@ -0,0 +1,13 @@
+import {ApiPromise} from '@polkadot/api';
+import type {Option} from '@polkadot/types';
+import type {AccountId, BalanceOf, ParaId} from '@polkadot/types/interfaces';
+import type {ITuple} from '@polkadot/types/types';
+import useApiQuery from 'src/api/hooks/useApiQuery';
+
+export function useParachainLeases(id: ParaId) {
+  return useApiQuery(['parachain_leases', id], async (api: ApiPromise) => {
+    const leases = (await api.query.slots?.leases?.(id)) as unknown as Option<ITuple<[AccountId, BalanceOf]>>[];
+
+    return leases.map((opt, index) => (opt.isSome ? index : -1)).filter((period) => period !== -1);
+  });
+}
