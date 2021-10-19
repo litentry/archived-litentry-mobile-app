5cffa5d5f22f33af93eee11e60b60ee0ba6f53b0
Francisco Silva <franjs.francisco@gmail.com>
2021-10-19 16:10:30 +0200

    Adding flow to sign transactions for internal accounts (#504)

--- src/context/TxContext/index.tsx
+++ src/context/TxContext/index.tsx
@@ -3,21 +3,24 @@ import {Dimensions, StyleSheet} from 'react-native';
 import {Modalize} from 'react-native-modalize';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import {ApiPromise} from '@polkadot/api';
+import {keyring} from '@polkadot/ui-keyring';
 import {SubmittableExtrinsic} from '@polkadot/api/submittable/types';
 import {SignerPayloadJSON, SignerResult} from '@polkadot/types/types';
 import {BN_ZERO} from '@polkadot/util';
 import {Icon, IconProps, Layout, Text} from '@ui-kitten/components';
 import {useApi} from 'context/ChainApiContext';
-import {PreviewStep} from 'context/TxContext/PreviewStep';
+import {TxPreview} from 'context/TxContext/TxPreview';
+import {AuthenticateView} from 'context/TxContext/AuthenticateView';
 import {get} from 'lodash';
 import ErrorDialog from 'presentational/ErrorDialog';
 import LoadingView from 'presentational/LoadingView';
 import SuccessDialog from 'presentational/SuccessDialog';
 import TxPayloadQr from 'presentational/TxPayloadQr';
 import {formatCallMeta} from 'src/packages/call_inspector/CallInspector';
-import QrSigner from 'src/service/QrSigner';
+import AsyncSigner from 'src/service/AsyncSigner';
 import globalStyles, {standardPadding} from 'src/styles';
 import WarningDialog from 'presentational/WarningDialog';
+import type {KeyringPair} from 'src/types';
 
 let id = 0;
 
@@ -43,18 +46,19 @@ export type StartConfig = {
 
 function TxContextProvider({children}: PropTypes): React.ReactElement {
   const modalRef = useRef<Modalize>(null);
-  const signatureRef = useRef<(value: SignerResult) => void>();
+  const signTransactionRef = useRef<(value: SignerResult) => void>();
+  const showPreviewRef = useRef<(txPayload: SignerPayloadJSON) => Promise<void>>();
   const [state, dispatch] = useReducer(reducer, initialState);
   const {api: apiPromise} = useApi();
 
   useEffect(() => {
-    if (!apiPromise?.isConnected && state.step === 'submitting') {
+    if (!apiPromise?.isConnected && state.view === 'submitting_view') {
       dispatch({
-        type: 'WARNING',
+        type: 'SHOW_WARNING',
         payload: 'The transaction was sent but you got disconnected from the chain. Please verify later.',
       });
     }
-  }, [apiPromise?.isConnected, state.step]);
+  }, [apiPromise?.isConnected, state.view]);
 
   const start = useCallback(async (config: StartConfig) => {
     const {api, txMethod, params, address} = config;
@@ -76,20 +80,55 @@ function TxContextProvider({children}: PropTypes): React.ReactElement {
     const args = meta?.args.map(({name}) => name).join(', ') || '';
     const title = `Sending transaction ${section}.${method}(${args})`;
     const description = formatCallMeta(meta);
+    const keyringPair = keyring.getPair(address);
+    const isExternal = Boolean(keyringPair.meta.isExternal);
+
+    showPreviewRef.current = async (txPayload: SignerPayloadJSON) => {
+      const info = await transaction.paymentInfo(address);
+      if (isExternal) {
+        dispatch({
+          type: 'SHOW_TX_PREVIEW',
+          payload: {
+            txPayload: txPayload,
+            params,
+            title,
+            description,
+            partialFee: info.partialFee.toNumber(),
+            isExternalAccount: true,
+          },
+        });
+      } else {
+        const signed = transaction.registry
+          .createType('ExtrinsicPayload', txPayload, {version: txPayload.version})
+          .sign(keyringPair);
+
+        dispatch({
+          type: 'SHOW_TX_PREVIEW',
+          payload: {
+            txPayload: txPayload,
+            params,
+            title,
+            description,
+            partialFee: info.partialFee.toNumber(),
+            isExternalAccount: false,
+            signature: {id: ++id, ...signed},
+          },
+        });
+      }
+    };
 
     try {
       const f = await transaction.signAsync(address, {
         nonce: -1,
         tip: BN_ZERO,
-        signer: new QrSigner(async (txPayload) => {
-          const info = await transaction.paymentInfo(address);
-          dispatch({
-            type: 'PREVIEW',
-            payload: {txPayload: txPayload, params, title, description, partialFee: info.partialFee.toNumber()},
-          });
-
+        signer: new AsyncSigner(async (txPayload) => {
+          if (!isExternal && keyringPair.isLocked) {
+            dispatch({type: 'SHOW_AUTHENTICATE_VIEW', payload: {txPayload, keyringPair}});
+          } else {
+            showPreviewRef.current?.(txPayload);
+          }
           return new Promise((resolve) => {
-            signatureRef.current = resolve;
+            signTransactionRef.current = resolve;
           });
         }),
       });
@@ -97,7 +136,7 @@ function TxContextProvider({children}: PropTypes): React.ReactElement {
       await new Promise((resolve, reject) => {
         f.send((result) => {
           if (result.isFinalized && !result.isError) {
-            dispatch({type: 'NEXT_STEP'});
+            dispatch({type: 'SHOW_SUCCESS_VIEW'});
             resolve(undefined);
           }
           if (result.isError && result.dispatchError) {
@@ -110,29 +149,39 @@ function TxContextProvider({children}: PropTypes): React.ReactElement {
             } else {
               error = result.dispatchError.toString();
             }
-            dispatch({type: 'ERROR', payload: error});
+            dispatch({type: 'SHOW_ERROR', payload: error});
             reject();
           }
         });
       });
     } catch (e) {
       console.warn('transaction error', e);
-      dispatch({type: 'ERROR', payload: String(e)});
+      dispatch({type: 'SHOW_ERROR', payload: String(e)});
     }
   }, []);
 
   const modalContent = useMemo(() => {
-    switch (state.step) {
-      case 'none':
+    switch (state.view) {
+      case 'initial_view':
         return (
           <Layout style={styles.emptyState}>
             <Text>Preparing transaction payload...</Text>
           </Layout>
         );
 
-      case 'preview':
+      case 'authenticate_view':
         return (
-          <PreviewStep
+          <AuthenticateView
+            keyringPair={state.keyringPair}
+            onAuthenticate={() => {
+              showPreviewRef.current?.(state.txPayload);
+            }}
+          />
+        );
+
+      case 'tx_preview':
+        return (
+          <TxPreview
             transactionInfo={state.description}
             transactionTitle={state.title}
             txPayload={state.txPayload}
@@ -142,11 +191,19 @@ function TxContextProvider({children}: PropTypes): React.ReactElement {
               modalRef.current?.close();
               dispatch({type: 'RESET'});
             }}
-            onConfirm={() => dispatch({type: 'NEXT_STEP'})}
+            isExternalAccount={state.isExternalAccount}
+            onConfirm={() => {
+              if (state.isExternalAccount) {
+                dispatch({type: 'SHOW_QR_CODE_TX_PAYLOAD_VIEW', payload: {txPayload: state.txPayload}});
+              } else {
+                dispatch({type: 'SHOW_SUBMITTING_VIEW'});
+                signTransactionRef.current?.(state.signature);
+              }
+            }}
           />
         );
 
-      case 'payload':
+      case 'qr_code_tx_payload_view':
         return (
           <TxPayloadQr
             payload={state.txPayload}
@@ -154,41 +211,17 @@ function TxContextProvider({children}: PropTypes): React.ReactElement {
               modalRef.current?.close();
               dispatch({type: 'RESET'});
             }}
-            onConfirm={() => dispatch({type: 'NEXT_STEP'})}
+            onConfirm={() => dispatch({type: 'SHOW_SCAN_SIGNATURE_VIEW'})}
           />
         );
 
-      case 'submitting':
+      case 'scan_signature_view':
         return (
-          <Layout style={styles.infoContainer}>
-            <LoadingView
-              text="Submitting"
-              renderIcon={() => <Icon name="cloud-upload-outline" fill="#ccc" style={styles.iconText} />}
-            />
-          </Layout>
-        );
-
-      case 'success':
-        return (
-          <Layout style={styles.infoContainer}>
-            <SuccessDialog
-              text="Tx Success"
-              onClosePress={() => {
-                modalRef.current?.close();
-                dispatch({type: 'RESET'});
-              }}
-            />
-          </Layout>
-        );
-
-      case 'signature':
-        return (
-          // TODO: extract
           <Layout style={globalStyles.paddedContainer}>
             <QRCodeScanner
               onRead={(data) => {
-                dispatch({type: 'NEXT_STEP'});
-                signatureRef.current?.({
+                dispatch({type: 'SHOW_SUBMITTING_VIEW'});
+                signTransactionRef.current?.({
                   id: id++,
                   signature: `0x${data.data}`,
                 });
@@ -213,14 +246,37 @@ function TxContextProvider({children}: PropTypes): React.ReactElement {
           </Layout>
         );
 
-      case 'error':
+      case 'submitting_view':
+        return (
+          <Layout style={styles.infoContainer}>
+            <LoadingView
+              text="Submitting"
+              renderIcon={() => <Icon name="cloud-upload-outline" fill="#ccc" style={styles.iconText} />}
+            />
+          </Layout>
+        );
+
+      case 'success_view':
+        return (
+          <Layout style={styles.infoContainer}>
+            <SuccessDialog
+              text="Tx Success"
+              onClosePress={() => {
+                modalRef.current?.close();
+                dispatch({type: 'RESET'});
+              }}
+            />
+          </Layout>
+        );
+
+      case 'error_view':
         return (
           <Layout style={styles.infoContainer}>
             <ErrorDialog text="Tx Failed" msg={state.error} />
           </Layout>
         );
 
-      case 'warning':
+      case 'warning_view':
         return (
           <Layout style={styles.infoContainer}>
             <WarningDialog text="Tx Sent" msg={state.warning} />
@@ -245,7 +301,7 @@ function TxContextProvider({children}: PropTypes): React.ReactElement {
         handlePosition="outside"
         onClosed={() => {
           dispatch({type: 'RESET'});
-          signatureRef.current = undefined;
+          signTransactionRef.current = undefined;
         }}
         closeOnOverlayTap
         panGestureEnabled>
@@ -321,64 +377,113 @@ const styles = StyleSheet.create({
 export default TxContextProvider;
 
 type State =
-  | {step: 'none' | 'signature' | 'submitting' | 'success'}
+  | {view: 'initial_view' | 'submitting_view' | 'success_view'}
+  | {view: 'authenticate_view'; txPayload: SignerPayloadJSON; keyringPair: KeyringPair}
   | {
-      step: 'preview';
+      view: 'tx_preview';
       txPayload: SignerPayloadJSON;
       params: unknown[];
       title: string;
       description: string;
       partialFee: number;
+      isExternalAccount: true;
     }
-  | {step: 'payload'; txPayload: SignerPayloadJSON}
-  | {step: 'error'; error: string}
-  | {step: 'warning'; warning: string};
+  | {
+      view: 'tx_preview';
+      txPayload: SignerPayloadJSON;
+      params: unknown[];
+      title: string;
+      description: string;
+      partialFee: number;
+      isExternalAccount: false;
+      signature: Signature;
+    }
+  | {view: 'qr_code_tx_payload_view'; txPayload: SignerPayloadJSON}
+  | {view: 'scan_signature_view'}
+  | {view: 'error_view'; error: string}
+  | {view: 'warning_view'; warning: string};
 
-const initialState: State = {step: 'none'};
+const initialState: State = {view: 'initial_view'};
+
+type Signature = {
+  id: number;
+  signature: string;
+};
 
 type Action =
   | {type: 'RESET'}
-  | {type: 'NEXT_STEP'}
-  | {type: 'ERROR'; payload: string}
-  | {type: 'WARNING'; payload: string}
   | {
-      type: 'PREVIEW';
+      type: 'SHOW_AUTHENTICATE_VIEW';
       payload: {
         txPayload: SignerPayloadJSON;
-        partialFee: number;
-        params: unknown[];
-        title: string;
-        description: string;
+        keyringPair: KeyringPair;
       };
-    };
+    }
+  | {
+      type: 'SHOW_TX_PREVIEW';
+      payload:
+        | {
+            txPayload: SignerPayloadJSON;
+            partialFee: number;
+            params: unknown[];
+            title: string;
+            description: string;
+            isExternalAccount: true;
+          }
+        | {
+            txPayload: SignerPayloadJSON;
+            partialFee: number;
+            params: unknown[];
+            title: string;
+            description: string;
+            isExternalAccount: false;
+            signature: Signature;
+          };
+    }
+  | {
+      type: 'SHOW_QR_CODE_TX_PAYLOAD_VIEW';
+      payload: {
+        txPayload: SignerPayloadJSON;
+      };
+    }
+  | {
+      type: 'SHOW_SCAN_SIGNATURE_VIEW';
+    }
+  | {type: 'SHOW_SUBMITTING_VIEW'}
+  | {type: 'SHOW_ERROR'; payload: string}
+  | {type: 'SHOW_WARNING'; payload: string}
+  | {type: 'SHOW_SUCCESS_VIEW'};
 
 function reducer(state: State, action: Action): State {
   switch (action.type) {
     case 'RESET':
       return initialState;
 
-    case 'ERROR':
-      return {step: 'error', error: action.payload};
+    case 'SHOW_AUTHENTICATE_VIEW':
+      return {view: 'authenticate_view', txPayload: action.payload.txPayload, keyringPair: action.payload.keyringPair};
 
-    case 'WARNING':
-      return {step: 'warning', warning: action.payload};
+    case 'SHOW_TX_PREVIEW':
+      return {view: 'tx_preview', ...action.payload};
 
-    case 'PREVIEW':
-      return {step: 'preview', ...action.payload};
+    case 'SHOW_QR_CODE_TX_PAYLOAD_VIEW':
+      return {view: 'qr_code_tx_payload_view', txPayload: action.payload.txPayload};
 
-    case 'NEXT_STEP': {
-      switch (state.step) {
-        case 'preview':
-          return {step: 'payload', txPayload: state.txPayload};
-        case 'payload':
-          return {step: 'signature'};
-        case 'signature':
-          return {step: 'submitting'};
-        case 'submitting':
-          return {step: 'success'};
-      }
-      break;
-    }
+    case 'SHOW_SCAN_SIGNATURE_VIEW':
+      return {view: 'scan_signature_view'};
+
+    case 'SHOW_SUBMITTING_VIEW':
+      return {view: 'submitting_view'};
+
+    case 'SHOW_SUCCESS_VIEW':
+      return {view: 'success_view'};
+
+    case 'SHOW_ERROR':
+      return {view: 'error_view', error: action.payload};
+
+    case 'SHOW_WARNING':
+      return {view: 'warning_view', warning: action.payload};
+
+    default:
+      return state;
   }
-  return state;
 }
