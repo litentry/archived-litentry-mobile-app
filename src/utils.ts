a883e2cc82a908310c47352891d3d415c6811184
Francisco Silva <franjs.francisco@gmail.com>
2021-12-06 14:38:56 +0100

    moving call functions to utils

--- src/utils.ts
+++ src/utils.ts
@@ -14,7 +14,7 @@ import {
 } from './constants';
 import {blake2AsU8a, checkAddress, decodeAddress, isEthereumChecksum} from '@polkadot/util-crypto';
 import {SignerPayloadJSON} from '@polkadot/types/types';
-import {ExtrinsicPayload} from '@polkadot/types/interfaces';
+import {ExtrinsicPayload, FunctionMetadataLatest} from '@polkadot/types/interfaces';
 import registry from 'src/typeRegistry';
 import {AccountAddressType, NetworkType} from './types';
 import {trim} from 'lodash';
@@ -211,4 +211,35 @@ export function notEmpty<TValue>(value: TValue | null | undefined | ''): value i
 
 export function getCurrentYear() {
   return new Date().getFullYear();
+}
+
+/**
+ * functions below help extract useful data
+ * from each motion object. they are mostly loosely copied from
+ * https://github.com/polkadot-js/apps/blob/master/packages/react-components/src/Call.tsx
+ */
+
+export function formatCallMeta(meta?: FunctionMetadataLatest): string {
+  if (!meta || !meta.docs.length) {
+    return '';
+  }
+
+  const strings = meta.docs.map((doc) => doc.toString().trim());
+  const firstEmpty = strings.findIndex((doc) => !doc.length);
+  const combined = (firstEmpty === -1 ? strings : strings.slice(0, firstEmpty))
+    .join(' ')
+    .replace(/#(<weight>| <weight>).*<\/weight>/, '');
+  const parts = splitParts(combined.replace(/\\/g, '').replace(/`/g, ''));
+
+  return parts.join(' ');
+}
+
+function splitSingle(value: string[], sep: string): string[] {
+  return value.reduce((result: string[], _value: string): string[] => {
+    return _value.split(sep).reduce((_result: string[], __value: string) => _result.concat(__value), result);
+  }, []);
+}
+
+function splitParts(value: string): string[] {
+  return ['[', ']'].reduce((result: string[], sep) => splitSingle(result, sep), [value]);
 }
