4acb9fbdae177a139bbc793c8b33a834341bfd82
Amir Hoseinian <hoseinian.amir@gmail.com>
2021-12-21 12:21:45 +0100

    Refactor dashboard & api loading to use same header
    Issue #704

--- src/ui/screens/ConnectionRetryScreen.tsx
+++ src/ui/screens/ConnectionRetryScreen.tsx
@@ -1,17 +1,15 @@
-import React, {useContext} from 'react';
-import {View, StyleSheet} from 'react-native';
-import globalStyles, {monofontFamily, standardPadding} from '@ui/styles';
-import NetworkItem from '@ui/components/NetworkItem';
 import {CompositeNavigationProp} from '@react-navigation/native';
-import {apiLoadingScreen, networkSelectionScreen} from '@ui/navigation/routeKeys';
-import {ApiLoadingStackParamList, RootStackParamList} from '@ui/navigation/navigation';
 import {StackNavigationProp} from '@react-navigation/stack';
-import {Padder} from '@ui/components/Padder';
-import {NetworkContext} from 'context/NetworkContext';
-import {useApiReconnect} from 'context/ChainApiContext';
-import {AppBar, AppHeader, Title, Icon, Button, Text, useTheme} from '@ui/library';
 import {Layout} from '@ui/components/Layout';
-import {noop} from 'lodash';
+import {Padder} from '@ui/components/Padder';
+import {Button, Icon, Text, useTheme} from '@ui/library';
+import {ApiLoadingStackParamList, RootStackParamList} from '@ui/navigation/navigation';
+import {apiLoadingScreen, networkSelectionScreen} from '@ui/navigation/routeKeys';
+import globalStyles, {monofontFamily, standardPadding} from '@ui/styles';
+import {useApiReconnect} from 'context/ChainApiContext';
+import {NetworkContext} from 'context/NetworkContext';
+import React, {useContext} from 'react';
+import {StyleSheet, View} from 'react-native';
 
 type PropTypes = {
   navigation: CompositeNavigationProp<
@@ -36,20 +34,6 @@ export function ConnectionRetryScreen({navigation}: PropTypes) {
 
   return (
     <Layout style={styles.container}>
-      <AppHeader style={{backgroundColor: colors.primary}}>
-        <AppBar.Action icon="menu" color="transparent" onPress={noop} />
-        <AppBar.Content
-          style={styles.contentContainer}
-          title={
-            <View style={styles.networkName}>
-              <Title>Litentry</Title>
-            </View>
-          }
-        />
-        <View style={[styles.networkSwitch, {backgroundColor: colors.background}]}>
-          {currentNetwork ? <NetworkItem item={currentNetwork} isConnected={false} /> : null}
-        </View>
-      </AppHeader>
       <View style={globalStyles.centeredContainer}>
         <View style={styles.textContainer}>
           <Icon name="access-point-network-off" color={colors.error} />
@@ -71,12 +55,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  contentContainer: {
-    marginRight: '12%',
-  },
-  networkName: {
-    alignItems: 'center',
-  },
   textContainer: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -86,17 +64,5 @@ const styles = StyleSheet.create({
   text: {
     fontFamily: monofontFamily,
     marginLeft: standardPadding,
-  },
-  titleContainer: {alignItems: 'center'},
-  networkSwitch: {
-    position: 'absolute',
-    right: standardPadding * 1.5,
-    bottom: 0,
-    paddingHorizontal: standardPadding,
-    paddingVertical: standardPadding / 2,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    height: 30,
-    justifyContent: 'center',
   },
 });
