e2c99a57e643e36651da7f9f82fed0740e65be14
Ishrak Imam <ishrak@litentry.com>
2021-12-28 12:59:13 +0100

    Add info text

--- src/ui/components/RegistrarSelectionModal.tsx
+++ src/ui/components/RegistrarSelectionModal.tsx
@@ -1,6 +1,6 @@
 import React, {useCallback, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
-import {Modal, Button, TextInput, Subheading, Caption} from '@ui/library';
+import {Modal, Button, TextInput, Subheading, Caption, Icon} from '@ui/library';
 import {RegistrarInfoWithIndex} from 'src/api/hooks/useRegistrars';
 import globalStyles, {standardPadding} from '@ui/styles';
 import {Padder} from '@ui/components/Padder';
@@ -42,8 +42,12 @@ function RegistrarSelectionModal({onSelect, visible, onClose}: PropTypes) {
         }}
       />
       <Padder scale={1} />
-      <Caption>{'Fee'}</Caption>
       <TextInput mode="outlined" dense disabled value={feeDisplay} placeholder="Fee for judgement" />
+      <Padder scale={0.5} />
+      <View style={styles.info}>
+        <Icon name="alert-circle-outline" size={20} />
+        <Caption style={styles.caption}>{`Fee paid to registrar to provide Judgement`}</Caption>
+      </View>
       <Padder scale={2} />
       <View style={styles.buttons}>
         <Button onPress={handleClose} mode="outlined">
@@ -62,6 +66,13 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
     marginBottom: standardPadding,
+  },
+  caption: {
+    marginTop: 0,
+    marginLeft: 3,
+  },
+  info: {
+    flexDirection: 'row',
   },
 });
 
