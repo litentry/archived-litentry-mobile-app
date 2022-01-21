368bdcd26106acbee8c977314eca07fcfc1c7dd5
Francisco Silva <franjs.francisco@gmail.com>
2022-01-21 11:51:16 +0100

    removing manual JSI package install on android

--- android/app/src/main/java/com/litentry/governance/MainApplication.java
+++ android/app/src/main/java/com/litentry/governance/MainApplication.java
@@ -4,15 +4,14 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.facebook.react.bridge.JSIModulePackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.litentry.governance.LitentryJSIPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -39,7 +38,7 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected JSIModulePackage getJSIModulePackage() {
-            return new LitentryJSIPackage();
+            return new ReanimatedJSIModulePackage();
         }
       };
 
