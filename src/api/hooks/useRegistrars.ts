9ab981907a51a63a82769e01456b3ac05e6b997b
Ishrak Imam <ishrak@litentry.com>
2022-02-09 12:57:02 +0100

    Filter out undefined values

--- src/api/hooks/useRegistrars.ts
+++ src/api/hooks/useRegistrars.ts
@@ -1,5 +1,6 @@
 import type {PalletIdentityRegistrarInfo} from '@polkadot/types/lookup';
 import useApiQuery from 'src/api/hooks/useApiQuery';
+import {notEmpty} from 'src/utils';
 
 export interface RegistrarInfoWithIndex extends PalletIdentityRegistrarInfo {
   index: number;
@@ -8,5 +9,8 @@ export interface RegistrarInfoWithIndex extends PalletIdentityRegistrarInfo {
 export function useRegistrars(): RegistrarInfoWithIndex[] {
   const {data: registrarsInfo} = useApiQuery('registrars', (api) => api.query.identity.registrars());
 
-  return (registrarsInfo || []).map((r) => r.unwrapOr(undefined)).map((r, index) => Object.assign({}, r, {index}));
+  return (registrarsInfo || [])
+    .map((r) => r.unwrapOr(undefined))
+    .filter(notEmpty)
+    .map((r, index) => Object.assign({}, r, {index}));
 }
