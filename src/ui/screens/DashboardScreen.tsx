c31a37c4276983f88e7882ee0b769cfcc3718339
Francisco Silva <franjs.francisco@gmail.com>
2022-02-14 16:03:00 +0100

    adding pull-to-refresh to the dashboard screen

--- src/ui/screens/DashboardScreen.tsx
+++ src/ui/screens/DashboardScreen.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {StyleSheet} from 'react-native';
 import {DrawerNavigationProp} from '@react-navigation/drawer';
 import {CompositeNavigationProp} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
@@ -6,12 +7,18 @@ import {BountySummaryTeaser} from '@ui/components/BountySummaryTeaser';
 import {CouncilSummaryTeaser} from '@ui/components/CouncilSummaryTeaser';
 import {DemocracySummaryTeaser} from '@ui/components/DemocracySummaryTeaser';
 import {TreasurySummaryTeaser} from '@ui/components/TreasurySummaryTeaser';
-import {ScrollView, StyleSheet} from 'react-native';
 import {DashboardStackParamList, DrawerParamList} from '@ui/navigation/navigation';
 import {bountiesScreen, councilScreen, democracyScreen, treasuryScreen} from '@ui/navigation/routeKeys';
 import {standardPadding} from '@ui/styles';
 import {Layout} from '@ui/components/Layout';
 import {Padder} from '@ui/components/Padder';
+import {ScrollView} from '@ui/components/ScrollView';
+import {DEMOCRACY_SUMMARY_QUERY} from 'src/api/hooks/useDemocracySummary';
+import {COUNCIL_SUMMARY_QUERY} from 'src/api/hooks/useCouncilSummary';
+import {BOUNTIES_SUMMARY_QUERY} from 'src/api/hooks/useBountiesSummary';
+
+// TODO: add treasury summary query when merged
+const refetchQueries = [DEMOCRACY_SUMMARY_QUERY, COUNCIL_SUMMARY_QUERY, BOUNTIES_SUMMARY_QUERY];
 
 type PropTypes = {
   navigation: CompositeNavigationProp<
@@ -23,7 +30,7 @@ type PropTypes = {
 function DashboardScreen({navigation}: PropTypes) {
   return (
     <Layout style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scrollView}>
+      <ScrollView contentContainerStyle={styles.scrollView} refetchQueries={refetchQueries}>
         <DemocracySummaryTeaser onPress={() => navigation.navigate(democracyScreen)} />
         <Padder scale={1} />
         <CouncilSummaryTeaser onPress={() => navigation.navigate(councilScreen)} />
