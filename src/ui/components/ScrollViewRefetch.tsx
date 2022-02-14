90e18bbf825c6df06defff43cf6f144967a790d5
Francisco Silva <franjs.francisco@gmail.com>
2022-02-14 16:19:29 +0100

    rename custom ScrollView to ScrollViewRefetch

--- src/ui/components/ScrollViewRefetch.tsx
+++ src/ui/components/ScrollViewRefetch.tsx
@@ -7,16 +7,16 @@ import {useTheme} from '@ui/library';
 type RNScrollViewProps = React.ComponentProps<typeof RNScrollView>;
 
 type Props = Omit<RNScrollViewProps, 'refreshControl'> & {
-  refetchQueries?: RefetchQueries;
+  refetchQueries: RefetchQueries;
 };
 
-export function ScrollView({children, refetchQueries, ...props}: Props) {
+export function ScrollViewRefetch({children, refetchQueries, ...props}: Props) {
   const {colors} = useTheme();
   const {refreshing, refetch} = useRefetch(refetchQueries);
 
-  const refreshControl = refetchQueries ? (
+  const refreshControl = (
     <RefreshControl refreshing={refreshing} onRefresh={refetch} tintColor={colors.primary} colors={[colors.primary]} />
-  ) : undefined;
+  );
 
   return (
     <RNScrollView {...props} refreshControl={refreshControl}>
