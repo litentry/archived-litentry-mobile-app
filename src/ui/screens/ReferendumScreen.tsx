5103ee14a90d905c1b90654e6b870d94d54c76ed
Ishrak Imam <ishrak@litentry.com>
2022-02-20 11:44:31 +0100

    Update proposal and referendum detail screen

--- src/ui/screens/ReferendumScreen.tsx
+++ src/ui/screens/ReferendumScreen.tsx
@@ -1,57 +1,33 @@
 import React, {useReducer} from 'react';
 import {StyleSheet, View, ScrollView} from 'react-native';
-import {BN, BN_ONE} from '@polkadot/util';
 import {RouteProp} from '@react-navigation/native';
-import {Button, Divider, Headline, Modal, Select, Subheading, Caption, TextInput} from '@ui/library';
+import {Button, Divider, Headline, Modal, Select, Subheading, Caption, TextInput, List, Text} from '@ui/library';
 import {Layout} from '@ui/components/Layout';
 import {useApi} from 'context/ChainApiContext';
 import {ProgressBar} from '@ui/components/ProgressBar';
 import SafeView, {noTopEdges} from '@ui/components/SafeView';
 import {SelectAccount} from '@ui/components/SelectAccount';
 import {useApiTx} from 'src/api/hooks/useApiTx';
-import {useBestNumber} from 'src/api/hooks/useBestNumber';
-import {useBlockTime} from 'src/api/hooks/useBlockTime';
 import {useConvictions, Conviction} from 'src/api/hooks/useConvictions';
-import {useDemocracy} from 'src/api/hooks/useDemocracy';
 import {useFormatBalance} from 'src/api/hooks/useFormatBalance';
 import {getBalanceFromString} from 'src/api/utils/balance';
 import {DashboardStackParamList} from '@ui/navigation/navigation';
 import {referendumScreen} from '@ui/navigation/routeKeys';
-import {formatCallMeta} from 'src/utils/callMetadata';
 import globalStyles, {standardPadding} from '@ui/styles';
 import {Padder} from '@ui/components/Padder';
 import {decimalKeypad} from 'src/utils';
 import MaxBalance from '@ui/components/MaxBalance';
+import {ProposalCallInfo} from '@ui/components/ProposalCallInfo';
 
 export function ReferendumScreen({route}: {route: RouteProp<DashboardStackParamList, typeof referendumScreen>}) {
   const startTx = useApiTx();
   const {api} = useApi();
-
   const [state, dispatch] = useReducer(reducer, initialState);
-
   const formatBalance = useFormatBalance();
-  const {data} = useDemocracy();
-  const referendum = data?.referendums.find((r) => r.index.toString() === route.params.index);
-  const proposal = referendum?.image?.proposal;
-
-  const bestNumber = useBestNumber();
-  const remainBlock = bestNumber ? referendum?.status.end.sub(bestNumber).isub(BN_ONE) : undefined;
-  const enactBlock = bestNumber ? referendum?.status.end.add(referendum.status.delay).sub(bestNumber) : undefined;
-  const {timeStringParts: remainingTime} = useBlockTime(remainBlock);
-  const {timeStringParts: activateTime} = useBlockTime(enactBlock);
-
+  const referendum = route.params.referendum;
   const convictions = useConvictions();
 
-  const {meta, method, section} = proposal?.registry.findMetaCall(proposal.callIndex) ?? {};
-
-  const ayePercentage =
-    referendum && !referendum.votedTotal.isZero()
-      ? referendum.allAye
-          .reduce((total: BN, {balance}) => total.add(balance), new BN(0))
-          .muln(10000)
-          .div(referendum.votedTotal)
-          .toNumber() / 100
-      : 0;
+  const title = `${referendum.method}.${referendum.section}`;
 
   const reset = () => {
     dispatch({type: 'RESET'});
@@ -89,62 +65,49 @@ export function ReferendumScreen({route}: {route: RouteProp<DashboardStackParamL
     <Layout style={globalStyles.flex}>
       <SafeView edges={noTopEdges}>
         <ScrollView contentContainerStyle={styles.container}>
-          <Headline>Proposal</Headline>
-          <Padder scale={0.5} />
-          <Divider />
-          {proposal ? (
-            <>
-              <Caption>{`${section}.${method}`}</Caption>
-              <Caption>{`${formatCallMeta(meta)}`}</Caption>
-              <Padder scale={1.5} />
-              <Subheading>Hash of the proposal</Subheading>
-              <Caption numberOfLines={1}>{String(proposal?.hash)}</Caption>
-            </>
-          ) : (
-            <>
-              <Subheading>Preimage</Subheading>
-              <Caption numberOfLines={1} ellipsizeMode="middle">
-                {String(referendum?.imageHash)}
-              </Caption>
-            </>
-          )}
+          <List.Item title={title} disabled left={() => <Headline>{referendum.index}</Headline>} />
+          <ProposalCallInfo proposal={referendum} />
+          <Padder scale={1} />
+
+          <Caption>{`Proposal Hash:`}</Caption>
+          <Text selectable>{referendum.hash}</Text>
 
           <View style={styles.row}>
             <View style={styles.center}>
-              <Caption>Time left to vote</Caption>
+              <Caption>{`Time left to vote`}</Caption>
               <Caption adjustsFontSizeToFit={true} numberOfLines={1}>
-                {remainingTime.slice(0, 2).join(' ')}
+                {referendum.endPeriod.slice(0, 2).join(' ')}
               </Caption>
             </View>
             <View style={styles.center}>
-              <Caption>Time left to activate</Caption>
+              <Caption>{`Time left to activate`}</Caption>
               <Caption adjustsFontSizeToFit={true} numberOfLines={1}>
-                {activateTime.slice(0, 2).join(' ')}
+                {referendum.activatePeriod.slice(0, 2).join(' ')}
               </Caption>
             </View>
           </View>
           <Padder scale={1} />
 
-          <Headline>Live Results</Headline>
+          <Headline>{`Live Results`}</Headline>
           <Padder scale={0.5} />
           <Divider />
           <Padder scale={0.5} />
-          <ProgressBar percentage={ayePercentage} requiredAmount={80} />
+          <ProgressBar percentage={referendum.ayePercent} requiredAmount={80} />
           <View style={styles.row}>
             <View style={styles.center}>
-              <Subheading>YES</Subheading>
-              <Caption>{referendum ? formatBalance(referendum.votedAye) : undefined}</Caption>
-              <Caption>{`${referendum?.voteCountAye} participants`}</Caption>
+              <Subheading>{`YES`}</Subheading>
+              <Caption>{referendum.formattedVotedAye}</Caption>
+              <Caption>{`${referendum.voteCountAye} participants`}</Caption>
             </View>
             <View style={styles.center}>
-              <Subheading>NO</Subheading>
-              <Caption>{referendum ? formatBalance(referendum.votedNay) : undefined}</Caption>
-              <Caption>{`${referendum?.voteCountNay} participants`}</Caption>
+              <Subheading>{`NO`}</Subheading>
+              <Caption>{referendum.formattedVotedNay}</Caption>
+              <Caption>{`${referendum.voteCountNay} participants`}</Caption>
             </View>
           </View>
           <Padder scale={1} />
 
-          <Headline>Vote!</Headline>
+          <Headline>{`Vote!`}</Headline>
           <Padder scale={0.5} />
           <Divider />
           <View style={styles.row}>
@@ -158,7 +121,7 @@ export function ReferendumScreen({route}: {route: RouteProp<DashboardStackParamL
         </ScrollView>
 
         <Modal visible={state.voting !== undefined} onDismiss={() => dispatch({type: 'RESET'})}>
-          <Caption>Vote with account</Caption>
+          <Caption>{`Vote with account`}</Caption>
           <SelectAccount
             onSelect={(account) => {
               dispatch({type: 'SELECT_ACCOUNT', payload: account.address});
@@ -166,7 +129,7 @@ export function ReferendumScreen({route}: {route: RouteProp<DashboardStackParamL
           />
           <Padder scale={1} />
 
-          <Caption>Vote Value</Caption>
+          <Caption>{`Vote Value`}</Caption>
           <TextInput
             dense
             mode="outlined"
@@ -181,7 +144,7 @@ export function ReferendumScreen({route}: {route: RouteProp<DashboardStackParamL
           <MaxBalance address={state.account} />
           <Padder scale={1} />
 
-          <Caption>Conviction</Caption>
+          <Caption>{`Conviction`}</Caption>
           <Padder scale={0.5} />
 
           <Select
@@ -210,7 +173,6 @@ export function ReferendumScreen({route}: {route: RouteProp<DashboardStackParamL
 const styles = StyleSheet.create({
   container: {
     padding: standardPadding * 2,
-    marginHorizontal: standardPadding,
   },
   row: {
     flexDirection: 'row',
