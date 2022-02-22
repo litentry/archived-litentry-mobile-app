db5647cdacc0e7b38f2091dde069a41ee319f1d4
Ishrak Imam <ishrak@litentry.com>
2022-02-22 14:39:59 +0100

    Fetch module election infor from Litentry graph

---
+++ src/api/hooks/useModuleElection.ts
@@ -0,0 +1,22 @@
+import {gql, useQuery} from '@apollo/client';
+import type {SubstrateChainModuleElection} from 'src/generated/litentryGraphQLTypes';
+
+export type ModuleElection = SubstrateChainModuleElection;
+
+const MODULE_ELECTION_QUERY = gql`
+  query getModuleElection {
+    substrateChainModuleElection {
+      module
+      hasElections
+    }
+  }
+`;
+
+export function useModuleElection() {
+  const {data, ...rest} = useQuery<{substrateChainModuleElection: ModuleElection}>(MODULE_ELECTION_QUERY);
+
+  return {
+    data: data?.substrateChainModuleElection,
+    ...rest,
+  };
+}
