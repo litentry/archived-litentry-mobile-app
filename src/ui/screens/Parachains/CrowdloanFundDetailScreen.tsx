507e2b4e51d3ad4820edf46863956eeaa9a291a1
Francisco Silva <franjs.francisco@gmail.com>
2022-03-01 16:11:51 +0100

    refactoring detail screen

--- src/ui/screens/Parachains/CrowdloanFundDetailScreen.tsx
+++ src/ui/screens/Parachains/CrowdloanFundDetailScreen.tsx
@@ -3,20 +3,13 @@ import {Linking, StyleSheet, View} from 'react-native';
 import {RouteProp} from '@react-navigation/core';
 import {Text, Button, Subheading, Headline} from '@ui/library';
 import {Layout} from '@ui/components/Layout';
-import {BlockTime} from '@ui/components/BlockTime';
 import {EmptyView} from '@ui/components/EmptyView';
 import LoadingView from '@ui/components/LoadingView';
 import SafeView, {noTopEdges} from '@ui/components/SafeView';
-import {useBestNumber} from 'src/api/hooks/useBestNumber';
-import {useContributions} from 'src/api/hooks/useContributions';
-import {useCrowdloanFundByParaId} from 'src/api/hooks/useFunds';
-import {useParachainsLeasePeriod} from 'src/api/hooks/useParachainsLeasePeriod';
 import {CrowdloansStackParamList} from '@ui/navigation/navigation';
-import {formatNumber} from '@polkadot/util';
-import AddressInlineTeaser from '@ui/components/AddressInlineTeaser';
-import {useFormatBalance} from 'src/api/hooks/useFormatBalance';
 import globalStyles, {standardPadding} from '@ui/styles';
-import {useParaEndpoints} from 'src/api/hooks/useParaEndpoints';
+import {useParachainCrowdloan} from 'src/api/hooks/useParachainCrowdloan';
+import {AccountTeaser} from '@ui/components/Account/AccountTeaser';
 
 type ScreenProps = {
   route: RouteProp<CrowdloansStackParamList, 'Fund Detail'>;
@@ -24,77 +17,54 @@ type ScreenProps = {
 
 export function CrowdloanFundDetailScreen({route}: ScreenProps) {
   const {paraId, title} = route.params;
-  const bestNumber = useBestNumber();
-  const formatBalance = useFormatBalance();
-  const {data: fund, isError, isLoading} = useCrowdloanFundByParaId(paraId);
-  const {data: contributions} = useContributions(paraId);
-  const {data: leasePeriod} = useParachainsLeasePeriod();
-  const endpoints = useParaEndpoints(paraId);
+  const {data, loading} = useParachainCrowdloan(paraId);
 
-  if (isError) {
-    return <Text>Something bad happened!</Text>;
-  }
-
-  if (isLoading) {
-    return <LoadingView />;
-  }
-
-  if (!fund) {
-    return <EmptyView />;
-  }
-
-  const {info, isCapped, isEnded, isWinner, firstSlot} = fund;
-  const {end, firstPeriod, lastPeriod, cap, raised, depositor} = info;
-  const blocksLeft = bestNumber && end.gt(bestNumber) ? end.sub(bestNumber) : null;
-  const isOngoing = !(isCapped || isEnded || isWinner) && leasePeriod?.currentPeriod.lte(firstSlot);
-
-  const status = (
-    <Text>
-      {fund.isWinner ? 'Winner' : blocksLeft ? (fund.isCapped ? 'Capped' : isOngoing ? 'Active' : 'Past') : 'Ended'}
-    </Text>
-  );
-  const timeLeft = blocksLeft ? <BlockTime blockNumber={blocksLeft} /> : null;
-  const leases = (
-    <Text>
-      {firstPeriod.eq(lastPeriod)
-        ? formatNumber(firstPeriod)
-        : `${formatNumber(firstPeriod)} - ${formatNumber(lastPeriod)}`}
-    </Text>
-  );
-  const count = <Text>{formatNumber(contributions?.contributorsHex.length)}</Text>;
-  const homepage = endpoints?.length ? endpoints[0]?.homepage : undefined;
-  const toHomepage = (url: string) => {
+  const toHomepage = React.useCallback((url: string) => {
     Linking.canOpenURL(url).then((supported) => {
       if (supported) {
         Linking.openURL(url);
       }
     });
-  };
+  }, []);
+
+  if (loading && !data) {
+    return <LoadingView />;
+  }
+
+  if (!data) {
+    return <EmptyView />;
+  }
 
   return (
     <SafeView edges={noTopEdges}>
       <Layout style={globalStyles.paddedContainer}>
         <Headline style={styles.title}>{title}</Headline>
         <Row label={'Index'}>
-          <Text>{formatNumber(paraId)}</Text>
+          <Text>{data.paraId}</Text>
         </Row>
         <Row label={'Depositor'}>
-          <AddressInlineTeaser address={String(depositor)} />
+          <AccountTeaser account={data.depositor.account} />
         </Row>
-        <Row label={'Ending'}>{timeLeft}</Row>
-        <Row label={'Status'}>{status}</Row>
-        <Row label={'Leases'}>{leases}</Row>
+        <Row label={'Ending'}>
+          <Text>{`${data.ending[0]} ${data.ending[1]}`}</Text>
+        </Row>
+        <Row label={'Status'}>
+          <Text>{data.status}</Text>
+        </Row>
+        <Row label={'Leases'}>
+          <Text>{`${data.firstPeriod} - ${data.lastPeriod}`}</Text>
+        </Row>
         <Row label={'Raised'}>
-          <Text numberOfLines={1} adjustsFontSizeToFit>{`${formatBalance(raised, {
-            isShort: true,
-          })} / ${formatBalance(cap, {isShort: true})}`}</Text>
+          <Text numberOfLines={1} adjustsFontSizeToFit>{`${data.formattedRaised} / ${data.formattedCap}`}</Text>
         </Row>
-        <Row label={'Contributors'}>{count}</Row>
-        {homepage ? (
+        <Row label={'Contributors'}>
+          <Text>{data.contribution.contribution.contributorsCount}</Text>
+        </Row>
+        {data.homepage ? (
           <Button
             icon="home"
             onPress={() => {
-              toHomepage(homepage);
+              toHomepage(String(data.homepage));
             }}>
             {`Homepage`}
           </Button>
