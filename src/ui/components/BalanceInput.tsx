72575ede96dbfde41ad745ebf9a09609617086f6
Abhishek <akanth1994@gmail.com>
2022-03-07 17:08:28 +0100

    BalanceInput component refactored

--- src/ui/components/BalanceInput.tsx
+++ src/ui/components/BalanceInput.tsx
@@ -6,41 +6,43 @@ import {decimalKeypad} from 'src/utils';
 import MaxBalance from './MaxBalance';
 import {getBalanceFromString} from 'src/api/utils/balance';
 import {ApiPromise} from '@polkadot/api';
+import type {Account} from 'src/api/hooks/useAccount';
 
 type PropTypes = {
   api?: ApiPromise;
-  account?: string;
-  dispatchType?: string;
-  onSelectDispatch: (dispatch: any) => any;
+  account?: Account;
+  onChangeBalance: (dispatch: string) => void;
 };
 
 export function BalanceInput(props: PropTypes) {
   const {colors} = useTheme();
-  const {api, account, dispatchType} = props;
+  const {api, account} = props;
   const [amount, setAmount] = useState<string>('');
   const [isMaximum, setIsMaximum] = useState<boolean>(true);
   const formatBalance = useFormatBalance();
   useEffect(() => {
-    if (dispatchType) props.onSelectDispatch({type: dispatchType, payload: amount});
-    else props.onSelectDispatch(amount);
-  }, [amount, dispatchType, props]);
+    amount !== '' && Number(amount) >= Number(account?.balance.total) ? setIsMaximum(false) : setIsMaximum(true);
+  }, [amount, account]);
   return (
     <>
       <TextInput
         dense
         style={styles.textInput}
-        theme={isMaximum ? {colors: {primary: colors.primary}} : {colors: {primary: colors.error}}}
+        theme={{colors: {primary: isMaximum ? colors.primary : colors.backdrop}}}
         mode="outlined"
         autoComplete="off"
         placeholder="Enter amount"
         keyboardType="decimal-pad"
         value={amount}
         onFocus={() => setAmount('')}
-        onChangeText={(nextValue: string) => setAmount(decimalKeypad(nextValue))}
+        onChangeText={(nextValue: string) => {
+          setAmount(decimalKeypad(nextValue));
+          props.onChangeBalance(decimalKeypad(nextValue));
+        }}
         contextMenuHidden={true}
         right={
           <TextInput.Affix
-            textStyle={{paddingTop: 10}}
+            textStyle={styles.affix}
             text={(api && formatBalance(getBalanceFromString(api, amount))) ?? ''}
           />
         }
@@ -52,7 +54,7 @@ export function BalanceInput(props: PropTypes) {
 
 const styles = StyleSheet.create({
   textInput: {height: 45},
-  affix: {paddingTop: 9},
+  affix: {paddingTop: 10},
 });
 
 export default BalanceInput;
