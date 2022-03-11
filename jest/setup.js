b7c5f9a8fd818de8f6cfd29c7ce6b3081f640479
Francisco Silva <franjs.francisco@gmail.com>
2022-03-11 16:04:18 +0100

    adding jest setup file

---
+++ jest/setup.js
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler/jestSetup';
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+
+  return Reanimated;
+});
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
