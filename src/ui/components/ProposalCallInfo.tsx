44cb56b545ab2c4828507604c87e54c4e8945afc
Ishrak Imam <ishrak@litentry.com>
2022-03-14 16:11:51 +0100

    Accommodate new data into ProposalCallInfo component

--- src/ui/components/ProposalCallInfo.tsx
+++ src/ui/components/ProposalCallInfo.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {Paragraph, Card, Caption} from '@ui/library';
 import {Padder} from '@ui/components/Padder';
-import type {DemocracyProposal, DemocracyReferendum} from 'src/api/hooks/useDemocracy';
+import type {DemocracyProposal, DemocracyReferendum, ProposalSubCall} from 'src/api/hooks/useDemocracy';
+import {standardPadding} from '@ui/styles';
 
 type ProposalInfoProps = {
   proposal: DemocracyProposal | DemocracyReferendum;
@@ -10,16 +11,68 @@ type ProposalInfoProps = {
 
 export function ProposalCallInfo({proposal}: ProposalInfoProps) {
   return (
-    <View>
-      <Paragraph>{proposal.meta}</Paragraph>
-      <Padder scale={1} />
-      <Card>
+    <>
+      {proposal.meta && (
+        <>
+          <Paragraph>{proposal.meta}</Paragraph>
+          <Padder scale={0.5} />
+        </>
+      )}
+      <Card mode="outlined">
         <Card.Content>
           {proposal.args?.map((arg) => (
-            <Caption key={arg.name}>{`${arg.name}: ${arg.value}`}</Caption>
+            <>
+              <Caption key={arg.name}>{`${arg.name}: ${arg.value}`}</Caption>
+              {arg.subCalls &&
+                arg.subCalls.map((subCall) => {
+                  if (subCall) {
+                    return <SubCall subCall={subCall} />;
+                  }
+                  return null;
+                })}
+            </>
           ))}
         </Card.Content>
       </Card>
-    </View>
+    </>
   );
 }
+
+type SubCallProps = {
+  subCall: ProposalSubCall;
+};
+
+function SubCall({subCall}: SubCallProps) {
+  const title = `${subCall.method}.${subCall.section}`;
+  return (
+    <Card mode="outlined" style={styles.subCallContainer}>
+      <Card.Content>
+        {subCall.meta && (
+          <>
+            <Paragraph>{subCall.meta}</Paragraph>
+            <Padder scale={0.5} />
+          </>
+        )}
+        <Caption>{title}</Caption>
+        {subCall.args?.map((arg) => (
+          <>
+            <Caption key={arg?.name}>{`${arg?.name}: ${arg?.value}`}</Caption>
+            {arg?.subCalls &&
+              arg.subCalls.map((nestedSubCall) => {
+                if (nestedSubCall) {
+                  return <SubCall subCall={nestedSubCall} />;
+                }
+                return null;
+              })}
+          </>
+        ))}
+      </Card.Content>
+    </Card>
+  );
+}
+
+const styles = StyleSheet.create({
+  subCallContainer: {
+    marginBottom: standardPadding,
+  },
+});
