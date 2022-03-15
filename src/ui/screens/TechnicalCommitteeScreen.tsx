04dfb40bfbeb27042894258e6cf1932503583743
Ishrak Imam <ishrak@litentry.com>
2022-03-15 17:12:28 +0100

    Added info text in tech committee screen

--- src/ui/screens/TechnicalCommitteeScreen.tsx
+++ src/ui/screens/TechnicalCommitteeScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import {Caption, Icon} from '@ui/library';
 import SafeView, {noTopEdges} from '@ui/components/SafeView';
 import {useTechnicalCommitteeSummary} from 'src/api/hooks/useTechnicalCommitteeSummary';
 import LoadingView from '@ui/components/LoadingView';
-import globalStyles from '@ui/styles';
+import globalStyles, {standardPadding} from '@ui/styles';
 import StatInfoBlock from '@ui/components/StatInfoBlock';
 import {AccountsList} from '@ui/components/Account/AccountsList';
+import {Padder} from '@ui/components/Padder';
 
 export function TechnicalCommitteeScreen() {
   const {data: technicalCommittee, loading} = useTechnicalCommitteeSummary();
@@ -16,6 +18,23 @@ export function TechnicalCommitteeScreen() {
         <LoadingView />
       ) : (
         <View style={globalStyles.paddedContainer}>
+          <View style={styles.infoContainer}>
+            <Icon name="alert-circle" size={20} />
+            <Padder scale={0.5} />
+            <Caption
+              style={
+                styles.textWrap
+              }>{`The Technical Committee can, along with the Council, produce emergency referenda, which are fast-tracked for voting and implementation.`}</Caption>
+          </View>
+          <View style={styles.infoContainer}>
+            <Icon name="alert-circle" size={20} />
+            <Padder scale={0.5} />
+            <Caption
+              style={
+                styles.textWrap
+              }>{`Members are added or removed from the Technical Committee via a simple majority vote of the Council.`}</Caption>
+          </View>
+
           <AccountsList
             accounts={technicalCommittee?.members || []}
             header={
@@ -33,3 +52,16 @@ export function TechnicalCommitteeScreen() {
     </SafeView>
   );
 }
+
+const styles = StyleSheet.create({
+  infoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: standardPadding,
+  },
+  textWrap: {
+    flex: 1,
+    flexWrap: 'wrap',
+    textAlign: 'justify',
+  },
+});
