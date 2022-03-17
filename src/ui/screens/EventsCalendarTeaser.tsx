3758347bf0fb1b26c8b413b60559bd8ccda27913
Abhishek <akanth1994@gmail.com>
2022-03-17 09:08:46 +0100

    EventsCalendarTeaser added

---
+++ src/ui/screens/EventsCalendarTeaser.tsx
@@ -0,0 +1,60 @@
+import {LoadingBox} from '@ui/components/LoadingBox';
+import {SectionTeaserContainer} from '@ui/components/SectionTeaserContainer';
+import {useCalendarEvents} from 'src/api/hooks/useCalendarEvents';
+import {View, FlatList, StyleSheet} from 'react-native';
+import React from 'react';
+import {Divider, Text, useTheme} from '@ui/library';
+import {standardPadding} from '@ui/styles';
+
+type Props = {
+  onPress: () => void;
+};
+
+export function EventsCalendarTeaser(props: Props) {
+  const {colors} = useTheme();
+  const {data: events, loading} = useCalendarEvents();
+  const topThreeEvents = events?.slice(0, 3);
+  const formateDate = (date: string) => {
+    const formatedDate = new Date(date);
+    const [eventDate, eventMonth] = [formatedDate.getDate(), formatedDate.getMonth()];
+    return eventDate.toString().concat('/').concat(eventMonth.toString());
+  };
+  return (
+    <SectionTeaserContainer onPress={props.onPress} title="Events Calender">
+      {loading && !topThreeEvents ? (
+        <LoadingBox />
+      ) : topThreeEvents ? (
+        <FlatList
+          ItemSeparatorComponent={Divider}
+          data={topThreeEvents}
+          keyExtractor={(item) => item.id}
+          renderItem={({item}) => (
+            <View style={styles.container}>
+              <View style={styles.dateContainer}>
+                <Text style={{color: colors.primary}}>{formateDate(item.date)}</Text>
+              </View>
+              <View style={styles.titleContainer}>
+                <Text>{item.title}</Text>
+              </View>
+            </View>
+          )}
+        />
+      ) : null}
+    </SectionTeaserContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    paddingVertical: standardPadding,
+  },
+  dateContainer: {
+    width: '10%',
+    justifyContent: 'center',
+  },
+  titleContainer: {
+    maxWidth: '90%',
+    paddingHorizontal: standardPadding,
+  },
+});
