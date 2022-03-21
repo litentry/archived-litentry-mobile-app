3c2ff68c796c0bfb51e00e44b1214c3aa8b31c62
Abhishek <akanth1994@gmail.com>
2022-03-21 16:22:14 +0100

    getFormatedDate to fetch the date with any given format

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -10,3 +10,15 @@ export function fromNow(date: string) {
 export function getCurrentYear() {
   return new Date().getFullYear();
 }
+
+export function getFormatedDate(date: string, format?: string) {
+  const currentDate = dayjs(date);
+  if (!format) {
+    return [currentDate.date(), currentDate.month().toString(), currentDate.year().toString()].join('/');
+  }
+  const formatValues = format.toUpperCase().split('/');
+  const mappedDates = formatValues.map((event) =>
+    event === 'DD' ? currentDate.date() : event === 'MM' ? currentDate.month() : currentDate.year(),
+  );
+  return mappedDates.join('/');
+}
