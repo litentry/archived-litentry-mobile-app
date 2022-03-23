4a1865aad0fd2008eaa22d1c7fe66a183cdefa9e
Abhishek <akanth1994@gmail.com>
2022-03-23 12:48:40 +0100

    adding navigation to account List Item

--- src/ui/components/Account/AccountListItem.tsx
+++ src/ui/components/Account/AccountListItem.tsx
@@ -5,12 +5,16 @@ import type {Account as SubstrateChainAccount} from 'src/api/hooks/useAccount';
 import globalStyles from '@ui/styles';
 import Identicon from '@polkadot/reactnative-identicon';
 import {Account} from './Account';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {AppStackParamList} from '@ui/navigation/navigation';
+import {memberDetailsScreen} from '@ui/navigation/routeKeys';
 
 type Props = {
   account: SubstrateChainAccount;
 };
 
 export function AccountListItem({account}: Props) {
+  const navigation = useNavigation<NavigationProp<AppStackParamList>>();
   return (
     <List.Item
       left={() => (
@@ -20,7 +24,10 @@ export function AccountListItem({account}: Props) {
       )}
       title={() => (
         <View style={globalStyles.justifyCenter}>
-          <Account account={account} />
+          <Account
+            account={account}
+            onPress={() => navigation.navigate(memberDetailsScreen, {address: account?.address})}
+          />
         </View>
       )}
     />
