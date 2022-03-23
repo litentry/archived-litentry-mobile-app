b904181bd9aea24139daf7d8631beced904023ed
Abhishek <akanth1994@gmail.com>
2022-03-23 12:50:00 +0100

    adding member details to AccountTeaser

--- src/ui/components/Account/AccountTeaser.tsx
+++ src/ui/components/Account/AccountTeaser.tsx
@@ -4,6 +4,10 @@ import Identicon from '@polkadot/reactnative-identicon';
 import {Padder} from '@ui/components/Padder';
 import {Account} from './Account';
 import type {Account as AccountType} from 'src/api/hooks/useAccount';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {AppStackParamList} from '@ui/navigation/navigation';
+import {memberDetailsScreen} from '@ui/navigation/routeKeys';
+import LoadingView from '@ui/components/LoadingView';
 
 type Props = {
   account: AccountType;
@@ -11,11 +15,13 @@ type Props = {
 };
 
 export function AccountTeaser({account, identiconSize = 20}: Props) {
+  const navigation = useNavigation<NavigationProp<AppStackParamList>>();
+
   return (
     <View style={styles.container}>
       <Identicon value={account.address} size={identiconSize} />
       <Padder scale={0.5} />
-      <Account account={account} />
+      <Account account={account} onPress={() => navigation.navigate(memberDetailsScreen, {address: account.address})} />
     </View>
   );
 }
