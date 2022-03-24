fc27e30e2df25b37de51028e867b8d00a2a8379f
Ishrak Imam <ishrak@litentry.com>
2022-03-24 22:15:33 +0100

    useCouncilAccounts hook

---
+++ src/hooks/useCouncilAccounts.ts
@@ -0,0 +1,15 @@
+import {useAccounts} from 'context/AccountsContext';
+import {useCouncil} from 'src/api/hooks/useCouncil';
+
+export function useCouncilAccounts() {
+  const {networkAccounts} = useAccounts();
+  const {data: council} = useCouncil();
+
+  if (networkAccounts.length === 0 || !council) {
+    return [];
+  }
+
+  return networkAccounts.filter((account) =>
+    council.members.some((member) => member.account.address === account.address),
+  );
+}
