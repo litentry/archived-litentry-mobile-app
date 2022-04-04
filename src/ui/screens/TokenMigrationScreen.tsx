7c9c19e4f288393aacab136f079e184db284c2d4
Francisco Silva <franjs.francisco@gmail.com>
2022-04-04 16:10:14 +0200

    Testing token migration

--- src/ui/screens/TokenMigrationScreen.tsx
+++ src/ui/screens/TokenMigrationScreen.tsx
@@ -1,30 +1,87 @@
 import React from 'react';
 import {View, Text, Button} from 'react-native';
-import {stringToHex} from '@polkadot/util';
-import {useWalletConnect} from 'context/WalletConnectProvider';
+import {useWeb3Wallet} from 'context/Web3WalletContext';
 
 export function TokenMigrationScreen() {
-  const connector = useWalletConnect();
-  const message = 'My email is test@email.com';
-  const msgParams = [
-    stringToHex(message), // Required
-    '0x838C543187312cc85592f43a35b03A7aCb8B273a', // Required
-  ];
+  const wallet = useWeb3Wallet();
+  const [isApproving, setApproving] = React.useState(false);
+  const [isSubmitting, setSubmitting] = React.useState(false);
+  const litmusAddress = '5DkLmfqQDZtDpN2SiEMvVeTJuq8zW7pan2c4LqN3EV6deB64'; // TODO: select address from internal accounts
+  const ethAddress = '0x838C543187312cc85592f43a35b03A7aCb8B273a'; // TODO: select address from web3 accounts
+  const hasApproved = wallet?.connectedAccount?.approved.isGreaterThan(0) ?? false;
+  const [txHash, setTxHash] = React.useState('');
 
-  console.log('Accounts: ', connector.accounts);
+  const handleApprove = React.useCallback(async () => {
+    if (!wallet?.isConnected) {
+      throw new Error('The wallet must be connected to call this method');
+    }
+    setApproving(true);
+    const result = await wallet.approveForMigration(ethAddress);
+    if (result?.ok) {
+      wallet.updateAccount(ethAddress);
+    }
+    if (result?.error) {
+      // TODO: handle error
+      console.error(result.error);
+    }
+    setApproving(false);
+  }, [wallet]);
+
+  const onSubmit = React.useCallback(async () => {
+    if (!wallet?.isConnected) {
+      throw new Error('The wallet must be connected to call this method');
+    }
+    setSubmitting(true);
+    const result = await wallet.depositForMigration(ethAddress, 1, litmusAddress);
+    if (result?.ok) {
+      wallet.updateAccount(ethAddress);
+      setTxHash(result.ok);
+    }
+    if (result?.error) {
+      // TODO: handle error
+      console.error(result.error);
+    }
+    setSubmitting(false);
+  }, [wallet]);
+
+  if (isApproving) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>approving...</Text>
+      </View>
+    );
+  }
+
+  if (isSubmitting) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>submitting...</Text>
+      </View>
+    );
+  }
+
+  if (txHash) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Your transaction has been successfully submitted.</Text>
+        <Text>View transaction {txHash} on Etherscan.io</Text>
+      </View>
+    );
+  }
 
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Token Migration</Text>
-      {!connector.connected ? (
-        <Button title="Connect" onPress={() => connector.connect()} />
+      {!wallet?.isConnected ? (
+        <>
+          <Button title="Connect Wallet" onPress={() => wallet.connect()} />
+        </>
       ) : (
         <View>
-          <Button title="Kill Session" onPress={() => connector.killSession()} />
-          <Button
-            title="Sign message"
-            onPress={() => connector.signPersonalMessage(msgParams).then((result) => console.log(result))}
-          />
+          <Button title="Disconnect Wallet" onPress={() => wallet.disconnect()} />
+          <Button title="Update account" onPress={() => wallet.updateAccount(ethAddress)} />
+          {hasApproved ? <Text>Approved!!</Text> : <Button title="Approve" onPress={handleApprove} />}
+          {hasApproved ? <Button title="deposit migration" onPress={onSubmit} /> : null}
         </View>
       )}
     </View>
