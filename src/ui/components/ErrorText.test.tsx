f8543e1c349e3afac02d867086ae01c428fff160
Abhishek <akanth1994@gmail.com>
2022-04-25 11:55:00 +0200

    PR fixes

--- src/ui/components/ErrorText.test.tsx
+++ src/ui/components/ErrorText.test.tsx
@@ -1,25 +1,43 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import React from 'react';
 import {create} from 'react-test-renderer';
 import {render} from 'src/testUtils';
 import {ErrorText} from './ErrorText';
 
-jest.doMock('./ErrorText', () => {
-  return {
-    ErrorTextMockComponent: jest.fn(() => <ErrorText>Mocked Error Text</ErrorText>),
-  };
-});
-const {ErrorTextMockComponent} = require('./ErrorText');
-
 describe('ErrorText component', () => {
   it('should render the prop component correctly', async () => {
-    const {getByTestId} = render(<ErrorTextMockComponent />);
+    const {getByTestId} = render(<ErrorText>Mocked Error Text</ErrorText>);
     expect(getByTestId('error_text').children.at(0)).toEqual('Mocked Error Text');
   });
 
   it('Testing render with snapshot', () => {
-    const errorTextComponent = create(<ErrorTextMockComponent />);
+    const errorTextComponent = create(<ErrorText>Mocked Error Text</ErrorText>);
     const tree = errorTextComponent.toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(tree).toMatchInlineSnapshot(`
+      <Text
+        style={
+          Array [
+            Object {
+              "color": "#000000",
+              "fontFamily": "System",
+              "fontWeight": "400",
+            },
+            Object {
+              "textAlign": "left",
+            },
+            Array [
+              Object {
+                "textAlign": "center",
+              },
+              Object {
+                "color": "#B00020",
+              },
+            ],
+          ]
+        }
+        testID="error_text"
+      >
+        Mocked Error Text
+      </Text>
+    `);
   });
 });
