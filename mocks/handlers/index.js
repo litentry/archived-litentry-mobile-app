7802bfc214994306db107a0ead28a57afb46e5ff
Abhishek <akanth1994@gmail.com>
2022-04-29 14:14:22 +0200

    adding accountHandler and chainInfoHandler

--- mocks/handlers/index.js
+++ mocks/handlers/index.js
@@ -1,10 +1,14 @@
-import {registrarsHandler} from './registrarsHandler';
-import {democracyHandler} from './democracyHandler';
+import {accountHandler} from './accountHandler';
+import {chainInfoHandler} from './chainInfoHandler';
 import {councilMotionsHandler} from './councilMotionsHandler';
+import {democracyHandler} from './democracyHandler';
+import {registrarsHandler} from './registrarsHandler';
 
 export const handlers = [
   registrarsHandler,
   democracyHandler,
   councilMotionsHandler,
+  accountHandler,
+  chainInfoHandler,
   // other handlers
 ];
