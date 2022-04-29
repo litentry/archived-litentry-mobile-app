677298d8b51be10f1e186a2d8e673ab71b23bfc0
Humblefool94 <Akanth1994@gmail.com>
2022-04-29 12:51:35 +0200

    Issue 763 events teaser test (#1098)
    Co-authored-by: Ishrak Imam <ishrak@litentry.com>

--- mocks/handlers/index.js
+++ mocks/handlers/index.js
@@ -1,10 +1,12 @@
 import {registrarsHandler} from './registrarsHandler';
 import {democracyHandler} from './democracyHandler';
 import {councilMotionsHandler} from './councilMotionsHandler';
+import {eventsCalendarHandler} from './eventsCalendarHandler';
 
 export const handlers = [
   registrarsHandler,
   democracyHandler,
   councilMotionsHandler,
+  eventsCalendarHandler,
   // other handlers
 ];
