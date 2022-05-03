9c3bedb7ecaefafd218470e1b0cfe64f5d1881aa
Abhishek <akanth1994@gmail.com>
2022-05-03 11:31:11 +0200

    UI-bug-fix

--- src/ui/screens/AddAccountScreen/AddressPreview.tsx
+++ src/ui/screens/AddAccountScreen/AddressPreview.tsx
@@ -40,7 +40,9 @@ function AddressInfoPreview(props: PropTypes) {
                 <Icon name="account" size={20} />
               </View>
             )}
-            right={() => <Caption>{accountInfo?.display ?? 'Untitled account'}</Caption>}
+            right={() => (
+              <Caption>{accountInfo?.display ? stringShorten(accountInfo?.display) : 'Untitled account'}</Caption>
+            )}
           />
           {accountInfo?.balance && (
             <List.Item
@@ -53,27 +55,29 @@ function AddressInfoPreview(props: PropTypes) {
               right={() => <Caption>{accountInfo.balance?.formattedFree}</Caption>}
             />
           )}
-          <List.Item
-            title="Judgment"
-            left={() => (
-              <View style={globalStyles.justifyCenter}>
-                <Icon name="hammer" size={20} />
-              </View>
-            )}
-            right={() =>
-              accountInfo?.registration?.judgements?.map((judgment, i) => {
-                if (judgment) {
-                  return (
-                    <JudgmentStatus
-                      key={i}
-                      registrationJudgement={judgment}
-                      hasParent={Boolean(accountInfo?.registration?.displayParent)}
-                    />
-                  );
-                }
-              })
-            }
-          />
+          {accountInfo?.registration?.judgements && accountInfo?.registration?.judgements.length > 0 ? (
+            <List.Item
+              title="Judgment"
+              left={() => (
+                <View style={globalStyles.justifyCenter}>
+                  <Icon name="hammer" size={20} />
+                </View>
+              )}
+              right={() =>
+                accountInfo?.registration?.judgements?.map((judgment, i) => {
+                  if (judgment) {
+                    return (
+                      <JudgmentStatus
+                        key={i}
+                        registrationJudgement={judgment}
+                        hasParent={Boolean(accountInfo?.registration?.displayParent)}
+                      />
+                    );
+                  }
+                })
+              }
+            />
+          ) : null}
           <List.Item
             title="Network"
             left={() => (
