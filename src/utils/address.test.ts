e0e445512f5096bfe24b507a1db80c92a0a342f9
Abhishek <akanth1994@gmail.com>
2022-05-05 15:18:20 +0200

    toShortAddress Updated

--- src/utils/address.test.ts
+++ src/utils/address.test.ts
@@ -8,11 +8,8 @@ const KUSAMA_ADDRESS = 'D3KNzSrENcvScLGYKdzMNu7LBcCsoVyZ8X8DDjSh6Sg6LXN';
 const LITMUS_ADDRESS = 'jcSKTfG892DzZbXJjm13MRNptERKHEDXpsE8ooUFPt9pz8KvJ';
 
 test('toShortAddress function test', () => {
-  const testCase = [
-    '12BxCyEpjmi3fz582Vs99y5o4khR9oRMQXhM9LUcAj4mKaYo',
-    '1HDgY7vpDjafR5NM8dbwm1b3Rrs4zATuSCHHbe7YgpKUKFw',
-  ];
-  const expected = ['12BxCy…4mKaYo', '1HDgY7…pKUKFw'];
+  const testCase = [POLKADOT_ADDRESS, KUSAMA_ADDRESS, LITMUS_ADDRESS];
+  const expected = ['1HDgY7…pKUKFw', 'D3KNzS…Sg6LXN', 'jcSKTf…pz8KvJ'];
   testCase.forEach((address, i) => {
     const decimalValue = toShortAddress(address);
     expect(decimalValue).toBe(expected[i]);
