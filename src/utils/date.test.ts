5951047a680dc2b1c512fa5a65435de0eb30a175
Abhishek <akanth1994@gmail.com>
2022-05-05 15:25:52 +0200

    Date test case updated

--- src/utils/date.test.ts
+++ src/utils/date.test.ts
@@ -1,11 +1,17 @@
-import {formatDate, fromNow} from './date';
+import {formatDate, fromNow, getCurrentYear} from './date';
 
 test('fromNow function', () => {
   const now = fromNow('12-12-2015');
   expect(now).toBe('6 years ago');
 });
 
+test('getCurrentYear function', () => {
+  const fullYear = new Date().getFullYear();
+  const currentYear = getCurrentYear();
+  expect(currentYear).toEqual(fullYear);
+});
+
 test('fromNow function', () => {
-  const now = formatDate('04-05-2022');
-  expect(now).toBe('05/04/2022');
+  const format_date = formatDate('04-05-2022');
+  expect(format_date).toBe('05/04/2022');
 });
