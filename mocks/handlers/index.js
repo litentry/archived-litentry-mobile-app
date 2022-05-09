41d45c1209da901aef9b1d97e442a7baf367d671
Humblefool94 <Akanth1994@gmail.com>
2022-05-09 20:44:51 +0530

    Issue 763 overviewscreen test case (#1108)
    [Skip CI]

--- mocks/handlers/index.js
+++ mocks/handlers/index.js
@@ -2,12 +2,14 @@ import {councilMotionsHandler} from './councilMotionsHandler';
 import {democracyHandler} from './democracyHandler';
 import {democracySummaryHandler} from './democracySummaryHandler';
 import {eventsCalendarHandler} from './eventsCalendarHandler';
+import {parachainSummaryHandler} from './parachainSummaryHandler';
 import {registrarsHandler} from './registrarsHandler';
 
 export const handlers = [
   registrarsHandler,
   democracyHandler,
   councilMotionsHandler,
+  parachainSummaryHandler,
   democracySummaryHandler,
   eventsCalendarHandler,
   // other handlers
