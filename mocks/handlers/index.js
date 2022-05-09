385614c855a21c2aa6792490e9a45b8dabf089f3
Humblefool94 <abhishek@litentry.com>
2022-05-09 17:22:21 +0200

    Revert "Issue 763 overviewscreen test case (#1108)"
    This reverts commit 41d45c1209da901aef9b1d97e442a7baf367d671.

--- mocks/handlers/index.js
+++ mocks/handlers/index.js
@@ -2,14 +2,12 @@ import {councilMotionsHandler} from './councilMotionsHandler';
 import {democracyHandler} from './democracyHandler';
 import {democracySummaryHandler} from './democracySummaryHandler';
 import {eventsCalendarHandler} from './eventsCalendarHandler';
-import {parachainSummaryHandler} from './parachainSummaryHandler';
 import {registrarsHandler} from './registrarsHandler';
 
 export const handlers = [
   registrarsHandler,
   democracyHandler,
   councilMotionsHandler,
-  parachainSummaryHandler,
   democracySummaryHandler,
   eventsCalendarHandler,
   // other handlers
