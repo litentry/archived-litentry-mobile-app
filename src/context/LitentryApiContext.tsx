78ab834937b841b41c3276d421078dba282f099e
Francisco Silva <franjs.francisco@gmail.com>
2022-05-10 11:36:33 +0200

    Disabling apollo cache persist (#1157)

--- src/context/LitentryApiContext.tsx
+++ src/context/LitentryApiContext.tsx
@@ -1,13 +1,10 @@
-import React, {useState, useCallback, useEffect, useContext} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import {ApolloClient, ApolloProvider, InMemoryCache, NormalizedCacheObject, createHttpLink} from '@apollo/client';
-import {CachePersistor, MMKVWrapper} from 'apollo3-cache-persist';
-import {MMKV} from 'react-native-mmkv';
 import {useNetwork} from 'context/NetworkContext';
 
 const LITENTRY_API_URI = 'https://graph.litentry.io/graphql';
 
 type LitentryApiContextType = {
-  clearCache: () => void;
   client: ApolloClient<NormalizedCacheObject>;
 };
 
@@ -15,14 +12,7 @@ const LitentryApiContext = React.createContext<LitentryApiContextType | undefine
 
 export function LitentryApiClientProvider({children}: {children: React.ReactNode}) {
   const [client, setClient] = useState<ApolloClient<NormalizedCacheObject>>();
-  const [persistor, setPersistor] = useState<CachePersistor<NormalizedCacheObject>>();
   const {currentNetwork} = useNetwork();
-
-  const clearCache = useCallback(() => {
-    if (persistor) {
-      persistor.purge();
-    }
-  }, [persistor]);
 
   useEffect(() => {
     const init = async () => {
@@ -42,16 +32,6 @@ export function LitentryApiClientProvider({children}: {children: React.ReactNode
           },
         },
       });
-      const storage = new MMKV({id: `apollo-cache-${currentNetwork.key}`});
-      const cachePersistor = new CachePersistor({
-        cache,
-        storage: new MMKVWrapper(storage),
-        debug: __DEV__,
-        key: `persistor-key-${currentNetwork.key}`,
-      });
-      await cachePersistor.restore();
-
-      setPersistor(cachePersistor);
       setClient(
         new ApolloClient({
           link: createHttpLink({
@@ -79,17 +59,9 @@ export function LitentryApiClientProvider({children}: {children: React.ReactNode
 
   return (
     <ApolloProvider client={client}>
-      <LitentryApiContext.Provider value={{clearCache, client}}>{children}</LitentryApiContext.Provider>
+      <LitentryApiContext.Provider value={{client}}>{children}</LitentryApiContext.Provider>
     </ApolloProvider>
   );
-}
-
-export function useClearCache() {
-  const context = useContext(LitentryApiContext);
-  if (!context) {
-    throw new Error('useClearCache must be used within LitentryApiClientProvider');
-  }
-  return {clearCache: context.clearCache};
 }
 
 export function useLitentryApiClient() {
