db759022c389fced3bd5774e6cdaf078480cf3bc
Abhishek <abhishek@litentry.com>
2022-05-10 11:28:11 +0200

    errortext testcase updated

--- src/ui/components/ErrorText.test.tsx
+++ src/ui/components/ErrorText.test.tsx
@@ -1,42 +1,49 @@
 import React from 'react';
-import {create} from 'react-test-renderer';
 import {render} from 'src/testUtils';
 import {ErrorText} from './ErrorText';
 
-describe('ErrorText component', () => {
-  it('should render the prop component correctly', async () => {
-    const {getByText} = render(<ErrorText>Mocked Error Text</ErrorText>);
-    expect(getByText('Mocked Error Text')).toBeTruthy();
-  });
+test('should render with children data', async () => {
+  const {findByText} = render(<ErrorText>Mocked Error Text</ErrorText>);
+  expect(await findByText('Mocked Error Text')).toBeTruthy();
+});
 
-  it('Testing render with snapshot', () => {
-    const errorTextComponent = create(<ErrorText>Mocked Error Text</ErrorText>);
-    const tree = errorTextComponent.toJSON();
-    expect(tree).toMatchInlineSnapshot(`
-      <Text
+test('snapshot match', () => {
+  const errorTextComponent = render(<ErrorText>Mocked Error Text</ErrorText>);
+  const tree = errorTextComponent.toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+      <View
+        collapsable={false}
+        pointerEvents="box-none"
         style={
-          Array [
-            Object {
-              "color": "#000000",
-              "fontFamily": "System",
-              "fontWeight": "400",
-            },
-            Object {
-              "textAlign": "left",
-            },
-            Array [
-              Object {
-                "textAlign": "center",
-              },
-              Object {
-                "color": "#B00020",
-              },
-            ],
-          ]
+          Object {
+            "flex": 1,
+          }
         }
       >
-        Mocked Error Text
-      </Text>
+        <Text
+          style={
+            Array [
+              Object {
+                "color": "#000000",
+                "fontFamily": "System",
+                "fontWeight": "400",
+              },
+              Object {
+                "textAlign": "left",
+              },
+              Array [
+                Object {
+                  "textAlign": "center",
+                },
+                Object {
+                  "color": "#E55047",
+                },
+              ],
+            ]
+          }
+        >
+          Mocked Error Text
+        </Text>
+      </View>
     `);
-  });
 });
