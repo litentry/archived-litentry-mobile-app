adf5d2c15e85b582670138742288c988504778f0
Abhishek <abhishek@litentry.com>
2022-05-11 17:12:03 +0200

    navigation verification testcase

--- src/ui/screens/Bounty/BountiesScreen.test.tsx
+++ src/ui/screens/Bounty/BountiesScreen.test.tsx
@@ -1,14 +1,22 @@
+import {NavigationProp} from '@react-navigation/native';
+import {DashboardStackParamList} from '@ui/navigation/navigation';
+import {addBountyScreen} from '@ui/navigation/routeKeys';
 import React from 'react';
-import {render, waitFor} from 'src/testUtils';
+import {render, waitFor, fireEvent} from 'src/testUtils';
 import {BountiesScreen} from './BountiesScreen';
 
+const navigation = {
+  navigate: () => jest.fn(),
+  goBack: () => jest.fn,
+} as unknown as NavigationProp<DashboardStackParamList, typeof addBountyScreen>;
+
 test('render the loading view when data is fetching', () => {
-  const {getByTestId} = render(<BountiesScreen />);
+  const {getByTestId} = render(<BountiesScreen navigation={navigation} />);
   expect(getByTestId('loading_view')).toBeTruthy();
 });
 
 test('render when the data is fetched', async () => {
-  const {getByText, getAllByText} = render(<BountiesScreen />);
+  const {getByText, getAllByText} = render(<BountiesScreen navigation={navigation} />);
   await waitFor(() => {
     expect(getByText('ORML Security Bounty')).toBeTruthy();
     expect(getByText('go')).toBeTruthy();
@@ -18,5 +26,29 @@ test('render when the data is fetched', async () => {
 
     expect(getAllByText('Active').length).toBe(3);
     expect(getAllByText('Proposed').length).toBe(2);
+
+    expect(getByText(/Add Bounty/i)).toBeDefined();
+  });
+});
+
+test('add bounty, bounty details and back button clicked', async () => {
+  const {getByText} = render(<BountiesScreen navigation={navigation} />);
+  await waitFor(() => {
+    const navigationSpy = jest.spyOn(navigation, 'navigate');
+
+    fireEvent.press(getByText('Add Bounty'));
+    expect(navigationSpy).toHaveBeenCalledTimes(1);
+
+    fireEvent.press(getByText('ORML Security Bounty'));
+    expect(navigationSpy).toHaveBeenCalledTimes(1);
+
+    fireEvent.press(getByText('go'));
+    expect(navigationSpy).toHaveBeenCalledTimes(1);
+
+    fireEvent.press(getByText('Polkascan Foundation Budget | Common Good Organization'));
+    expect(navigationSpy).toHaveBeenCalledTimes(1);
+
+    fireEvent.press(getByText('Polkadot Pioneers Prize, an Incentive Prize Program'));
+    expect(navigationSpy).toHaveBeenCalledTimes(1);
   });
 });
