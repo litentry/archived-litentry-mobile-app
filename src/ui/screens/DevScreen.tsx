4d820a4f2168c05dc5ca763e234fdf32a7695fd1
Ishrak Imam <ishrak@litentry.com>
2022-05-12 22:07:32 +0200

    Refactor dev screen modalize

--- src/ui/screens/DevScreen.tsx
+++ src/ui/screens/DevScreen.tsx
@@ -1,18 +1,13 @@
-import React, {useCallback, useContext, useRef, useState} from 'react';
-import {Alert, StyleSheet, View} from 'react-native';
-import {Divider, Button, List, Subheading, useTheme, Paragraph, Caption, Select} from '@ui/library';
-import {Layout} from '@ui/components/Layout';
-import {useAccounts} from 'src/context/AccountsContext';
+import React, {useContext, useState} from 'react';
+import {View} from 'react-native';
+import {Divider, Button, List, Subheading, useTheme, Select} from '@ui/library';
 import {InAppNotificationContent, InAppNotificationContext} from 'context/InAppNotificationContext';
-import {ChainApiContext} from 'context/ChainApiContext';
+import {useApi} from 'context/ChainApiContext';
 import {ScrollView} from 'react-native-gesture-handler';
-import {Modalize} from 'react-native-modalize';
 import globalStyles, {standardPadding} from '@ui/styles';
 import {useNetwork} from 'context/NetworkContext';
 import RegistrarSelectionModal from '@ui/components/RegistrarSelectionModal';
 import SafeView, {noTopEdges} from '@ui/components/SafeView';
-import {stringShorten} from '@polkadot/util';
-import {Padder} from '@ui/components/Padder';
 import {useConvictions} from 'src/api/hooks/useConvictions';
 
 function DevScreen() {
@@ -20,16 +15,9 @@ function DevScreen() {
   const [visible, setVisible] = useState(false);
 
   const {currentNetwork} = useNetwork();
-  const {accounts} = useAccounts();
   const {data: convictions} = useConvictions();
   const {trigger} = useContext(InAppNotificationContext);
-  const {status, api} = useContext(ChainApiContext);
-  const [debugInfo, setDebugInfo] = useState('');
-  const modalRef = useRef<Modalize>(null);
-  const showDebugModal = useCallback((info: string) => {
-    modalRef.current?.open();
-    setDebugInfo(info);
-  }, []);
+  const {status} = useApi();
 
   return (
     <SafeView edges={noTopEdges}>
@@ -39,7 +27,7 @@ function DevScreen() {
           description={currentNetwork.ws}
           right={() => (
             <ItemRight>
-              <Subheading style={{color: colors.success}}>{status}</Subheading>
+              <Subheading style={{color: colors.success, marginRight: standardPadding * 2}}>{status}</Subheading>
             </ItemRight>
           )}
         />
@@ -112,52 +100,7 @@ function DevScreen() {
           )}
         />
         <Divider />
-
-        {Object.values(accounts).map((account) => (
-          <View key={account.address}>
-            <List.Item
-              title={<Paragraph>{`Identity of ${stringShorten(account.address)}`}</Paragraph>}
-              description="Resp of `identityOf` call of current address"
-              right={() => (
-                <ItemRight>
-                  <Button
-                    mode="contained"
-                    onPress={() => {
-                      if (account) {
-                        api?.query.identity?.identityOf(account.address).then((data) => {
-                          showDebugModal(JSON.stringify(data, null, 4));
-                        });
-                      } else {
-                        Alert.alert('Error', 'No Account connected');
-                      }
-                    }}>
-                    Trigger
-                  </Button>
-                </ItemRight>
-              )}
-            />
-            <Divider />
-          </View>
-        ))}
       </ScrollView>
-
-      <Modalize
-        ref={modalRef}
-        threshold={250}
-        scrollViewProps={{showsVerticalScrollIndicator: false}}
-        adjustToContentHeight
-        handlePosition="outside"
-        closeOnOverlayTap
-        panGestureEnabled>
-        <Layout style={styles.devModal}>
-          <Subheading>Debug Info</Subheading>
-          <Padder scale={0.5} />
-          <Divider />
-          <ScrollView style={styles.scrollView}>
-            <Caption>{debugInfo}</Caption>
-          </ScrollView>
-        </Layout>
-      </Modalize>
     </SafeView>
   );
 }
@@ -165,23 +108,5 @@ function DevScreen() {
 function ItemRight({children}: {children: React.ReactNode}) {
   return <View style={globalStyles.justifyCenter}>{children}</View>;
 }
-
-const styles = StyleSheet.create({
-  devModal: {
-    maxHeight: 400,
-    height: 400,
-    padding: standardPadding,
-  },
-  debugHeader: {
-    paddingVertical: standardPadding * 2,
-    justifyContent: 'center',
-    alignSelf: 'center',
-  },
-  scrollView: {
-    padding: standardPadding,
-    flex: 1,
-    height: 400,
-  },
-});
 
 export default DevScreen;
