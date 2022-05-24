ab072a27ea29d98f2d92dc641d055be821aedaf9
Abhishek <abhishek@litentry.com>
2022-05-24 10:51:01 +0200

    yarn lint

--- src/ui/screens/AddAccountScreen/AddressPreview.test.tsx
+++ src/ui/screens/AddAccountScreen/AddressPreview.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, waitFor, within} from 'src/testUtils';
+import {render, waitFor} from 'src/testUtils';
 import {NetworkType} from 'src/types';
 import AddressInfoPreview from './AddressPreview';
 
