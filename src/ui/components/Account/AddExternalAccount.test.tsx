b340341413121acb9bffb80ae4c39ecbfef872e1
Abhishek <abhishek@litentry.com>
2022-06-06 20:08:44 +0200

    Cancel testcase added

--- src/ui/components/Account/AddExternalAccount.test.tsx
+++ src/ui/components/Account/AddExternalAccount.test.tsx
@@ -15,10 +15,18 @@ test('render the AddExternalAccount component, test for wrong address', () => {
 
 test('render the AddExternalAccount component, with right address', () => {
   const {getByPlaceholderText, getByText, getByA11yState} = render(<AddExternalAccount onClose={closeModel} />);
+  getByText('Add External Account');
   const inputAddress = getByPlaceholderText('👉 Paste address here, e.g. 167r...14h');
   expect(inputAddress).toBeTruthy();
   getByA11yState({disabled: true});
   fireEvent.changeText(inputAddress, '14yx4vPAACZRhoDQm1dyvXD3QdRQyCRRCe5tj1zPomhhS29a');
   expect(getByText(/Confirm/i)).toBeDefined();
   fireEvent.press(getByText(/Confirm/i));
+});
+
+test('render the AddExternalAccount component, pressed cancel', () => {
+  const {getByText, debug} = render(<AddExternalAccount onClose={closeModel} />);
+  getByText('Add External Account');
+  expect(getByText(/Cancel/i)).toBeDefined();
+  fireEvent.press(getByText(/Cancel/i));
 });
