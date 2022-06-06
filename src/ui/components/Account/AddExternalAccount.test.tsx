1e9c4065615c43537b8e91cf25044e593b8b8341
Abhishek <abhishek@litentry.com>
2022-06-06 20:22:20 +0200

    updated testcase

--- src/ui/components/Account/AddExternalAccount.test.tsx
+++ src/ui/components/Account/AddExternalAccount.test.tsx
@@ -14,19 +14,25 @@ test('render the AddExternalAccount component, test for wrong address', () => {
 });
 
 test('render the AddExternalAccount component, with right address', () => {
-  const {getByPlaceholderText, getByText, getByA11yState} = render(<AddExternalAccount onClose={closeModel} />);
+  const {getByPlaceholderText, getByText, getByA11yState, getAllByA11yRole} = render(
+    <AddExternalAccount onClose={closeModel} />,
+  );
   getByText('Add External Account');
+  const confirm = getAllByA11yRole('button')[3];
   const inputAddress = getByPlaceholderText('👉 Paste address here, e.g. 167r...14h');
   expect(inputAddress).toBeTruthy();
-  getByA11yState({disabled: true});
+  expect(confirm).toBeDisabled();
   fireEvent.changeText(inputAddress, '14yx4vPAACZRhoDQm1dyvXD3QdRQyCRRCe5tj1zPomhhS29a');
-  expect(getByText(/Confirm/i)).toBeDefined();
-  fireEvent.press(getByText(/Confirm/i));
+  expect(confirm).toHaveTextContent('Confirm');
+  expect(confirm).toBeEnabled();
 });
 
-test('render the AddExternalAccount component, pressed cancel', () => {
-  const {getByText, debug} = render(<AddExternalAccount onClose={closeModel} />);
+test('render the AddExternalAccount component', () => {
+  const {getByText, getAllByA11yRole} = render(<AddExternalAccount onClose={closeModel} />);
   getByText('Add External Account');
-  expect(getByText(/Cancel/i)).toBeDefined();
-  fireEvent.press(getByText(/Cancel/i));
+  const cancel = getAllByA11yRole('button')[2];
+  expect(cancel).toHaveTextContent('Cancel');
+  const confirm = getAllByA11yRole('button')[3];
+  expect(confirm).toHaveTextContent('Confirm');
+  expect(confirm).toBeDisabled();
 });
