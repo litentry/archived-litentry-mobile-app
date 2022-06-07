9ab7da377b78e69e2e9b7ec667811fcbcb04a92b
Abhishek <abhishek@litentry.com>
2022-06-07 12:49:17 +0200

    added handler to index

--- mocks/handlers/index.js
+++ mocks/handlers/index.js
@@ -3,10 +3,13 @@ import {bountiesHandler} from './bountiesHandler';
 import {bountiesSummaryHandler} from './bountiesSummaryHandler';
 import {bountyHandler} from './bountyHandler';
 import {chainInfoHandler} from './chainInfoHandler';
+import {councilHandler} from './councilHandler';
 import {councilMotionsHandler} from './councilMotionsHandler';
+import {councilVotesHandler} from './councilVotesHandler';
 import {democracyHandler} from './democracyHandler';
 import {democracySummaryHandler} from './democracySummaryHandler';
 import {eventsCalendarHandler} from './eventsCalendarHandler';
+import {moduleElectionHandler} from './moduleElectionHandler';
 import {parachainSummaryHandler} from './parachainSummaryHandler';
 import {parathreadsHandler} from './parathreadHandler';
 import {registrarsHandler} from './registrarsHandler';
@@ -26,5 +29,8 @@ export const handlers = [
   bountyHandler,
   chainInfoHandler,
   accountHandler,
+  councilHandler,
+  moduleElectionHandler,
+  councilVotesHandler,
   // other handlers
 ];
