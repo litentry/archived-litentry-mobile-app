63adeb3496e99ce6c2bf20fac1daa5780ded3a85
Abhishek <abhishek@litentry.com>
2022-06-12 03:11:17 +0200

    account handler updated

--- mocks/handlers/accountHandler.js
+++ mocks/handlers/accountHandler.js
@@ -4,10 +4,13 @@ export const accountHandler = graphql.query('getAccount', (_, res, ctx) => {
   return res(
     ctx.data({
       substrateChainAccount: {
+        __typename: 'SubstrateChainAccount',
+        subAccounts: [],
         address: '14yx4vPAACZRhoDQm1dyvXD3QdRQyCRRCe5tj1zPomhhS29a',
         display: 'PureStake/01',
         hasIdentity: true,
         registration: {
+          __typename: 'SubstrateChainDeriveAccountRegistration',
           display: '01',
           displayParent: 'PureStake',
           email: 'info@purestake.com',
@@ -19,8 +22,10 @@ export const accountHandler = graphql.query('getAccount', (_, res, ctx) => {
           web: 'https://www.purestake.com/',
           judgements: [
             {
+              __typename: 'SubstrateChainRegistrationJudgement',
               registrarIndex: 1,
               judgement: {
+                __typename: 'SubstrateChainIdentityJudgement',
                 isUnknown: false,
                 isFeePaid: false,
                 isReasonable: true,
@@ -33,14 +38,15 @@ export const accountHandler = graphql.query('getAccount', (_, res, ctx) => {
           ],
         },
         balance: {
-          total: '182675396161976',
-          formattedTotal: '18,267.5396 DOT',
+          __typename: 'SubstrateChainAccountBalance',
+          total: '196506492123851',
+          formattedTotal: '19,650.6492 DOT',
           reserved: '200410000000',
           formattedReserved: '20.0410 DOT',
-          free: '182474986161976',
-          formattedFree: '18,247.4986 DOT',
-          freeFrozen: '181215680471934',
-          formattedFreeFrozen: '18,121.5680 DOT',
+          free: '196306082123851',
+          formattedFree: '19,630.6082 DOT',
+          feeFrozen: '194996884114259',
+          formattedFeeFrozen: '19,499.6884 DOT',
         },
       },
     }),
