13bb911f6d61853952a83ac475cb315bfc4122c6
Francisco Silva <franjs.francisco@gmail.com>
2022-06-17 17:51:00 +0200

    adding TODO

--- src/polkadotApi/useAppAccounts.ts
+++ src/polkadotApi/useAppAccounts.ts
@@ -7,6 +7,7 @@ export function useAppAccounts() {
   const {currentNetwork} = useNetwork();
   const [accounts, setAccounts] = useRecoilState(appAccountsState);
 
+  // TODO: create a selector for networkAccounts (the currentNetwork needs to be moved to a atom)
   const networkAccounts = useMemo(() => {
     return Object.values(accounts).filter((account) => account.meta.network === currentNetwork.key);
   }, [accounts, currentNetwork]);
