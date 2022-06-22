270783831de6bbc8a9eeef3de8ea69d609459a01
Abhishek <abhishek@litentry.com>
2022-06-22 14:21:28 +0200

    updated CandidateScreen testcase

--- src/ui/screens/Council/CandidateScreen.test.tsx
+++ src/ui/screens/Council/CandidateScreen.test.tsx
@@ -1,10 +1,11 @@
 import {AppStackParamList, DashboardStackParamList} from '@ui/navigation/navigation';
 import {NavigationProp, RouteProp} from '@react-navigation/native';
 import React from 'react';
-import {render, waitFor, fireEvent, cleanup} from 'src/testUtils';
+import {render, fireEvent} from 'src/testUtils';
 import {CandidateScreen} from './CandidateScreen';
 import {Linking} from 'react-native';
 import {ReactTestInstance} from 'react-test-renderer';
+import {accountScreen} from '@ui/navigation/routeKeys';
 
 const navigation = {
   navigate: () => jest.fn(),
@@ -27,71 +28,59 @@ const route = {
   },
 } as unknown as RouteProp<DashboardStackParamList, 'Candidate'>;
 
-test('should render loading state when fetching data', () => {
-  const {getByTestId} = render(<CandidateScreen navigation={navigation} route={route} />);
-  expect(getByTestId('loading_view')).toBeTruthy();
-});
-
-test('should render component when the data is fetched', async () => {
-  const {getByText} = render(<CandidateScreen navigation={navigation} route={route} />);
-  await waitFor(() => {
-    expect(getByText('Legal')).toBeTruthy();
-    expect(getByText('Email')).toBeTruthy();
-    expect(getByText('Twitter')).toBeTruthy();
-    expect(getByText('Riot')).toBeTruthy();
-    expect(getByText('Web')).toBeTruthy();
-    expect(getByText('Backing')).toBeTruthy();
-    expect(getByText('Voters')).toBeTruthy();
-  });
-});
-
-test('twitter navigation', async () => {
+describe('candidateScreen', () => {
   const openURLSpy = jest.spyOn(Linking, 'openURL');
-  const {getByText} = render(<CandidateScreen navigation={navigation} route={route} />);
-  await waitFor(() => {
-    expect(getByText('Twitter')).toBeTruthy();
-    fireEvent.press(getByText('@nachortti'));
-    expect(openURLSpy).toBeCalled();
-  });
-});
 
-test('riot navigation', async () => {
-  const openURLSpy = jest.spyOn(Linking, 'openURL');
-  const {getByText} = render(<CandidateScreen navigation={navigation} route={route} />);
-  await waitFor(() => {
-    expect(getByText('Riot')).toBeTruthy();
-    fireEvent.press(getByText('@raul.rtti:matrix.parity.io'));
-    expect(openURLSpy).toBeCalled();
-  });
-});
-
-test('web navigation', async () => {
-  const openURLSpy = jest.spyOn(Linking, 'openURL');
-  const {getByText} = render(<CandidateScreen navigation={navigation} route={route} />);
-  await waitFor(() => {
-    expect(getByText('Web')).toBeTruthy();
-    fireEvent.press(getByText('www.nachortti.com'));
-    expect(openURLSpy).toBeCalled();
-  });
-});
-
-test('web navigation', async () => {
   const navigateSpy = jest.spyOn(navigation, 'navigate');
-  const {getByText} = render(<CandidateScreen navigation={navigation} route={route} />);
-  await waitFor(() => {
-    expect(getByText('RTTI-5220 (POLKADOT)')).toBeTruthy();
-    fireEvent.press(getByText('RTTI-5220 (POLKADOT)'));
-    expect(navigateSpy).toBeCalled();
-  });
-});
 
-test('account navigation', async () => {
-  const navigateSpy = jest.spyOn(navigation, 'navigate');
-  const {getAllByTestId} = render(<CandidateScreen navigation={navigation} route={route} />);
-  await waitFor(() => {
-    const accountDetails = getAllByTestId('accountsDetails') as ReactTestInstance[];
+  const accountAddress = '13RDY9nrJpyTDBSUdBw12dGwhk19sGwsrVZ2bxkzYHBSagP2';
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should render the loading screen when the data is fetching', () => {
+    const {getByTestId} = render(<CandidateScreen navigation={navigation} route={route} />);
+    expect(getByTestId('loading_view')).toBeTruthy();
+  });
+
+  it('should render the component after data is fetched with initial states ', async () => {
+    const {findByText, queryByText} = render(<CandidateScreen navigation={navigation} route={route} />);
+    await findByText('Legal');
+    await findByText('Email');
+    await findByText('Twitter');
+    await findByText('Riot');
+    await findByText('Web');
+    await findByText('Backing');
+    await findByText('Voters');
+    expect(queryByText('No voters yet.')).not.toBe(null);
+  });
+
+  it('should navigate to the linked twitter url', async () => {
+    const {findByText} = render(<CandidateScreen navigation={navigation} route={route} />);
+    fireEvent.press(await findByText('@nachortti'));
+    expect(openURLSpy).toHaveBeenCalledWith('https://twitter.com/@nachortti');
+  });
+
+  it('should navigate to the linked riot url', async () => {
+    const {findByText} = render(<CandidateScreen navigation={navigation} route={route} />);
+    fireEvent.press(await findByText('@raul.rtti:matrix.parity.io'));
+    expect(openURLSpy).toHaveBeenCalledWith('https://matrix.to/#/@raul.rtti:matrix.parity.io');
+  });
+
+  it('should navigate to the linked web url', async () => {
+    const {findByText} = render(<CandidateScreen navigation={navigation} route={route} />);
+    fireEvent.press(await findByText('www.nachortti.com'));
+    expect(openURLSpy).toHaveBeenCalledWith('www.nachortti.com');
+  });
+
+  it('should navigate to the account details page when clicked account', async () => {
+    const {findAllByTestId} = render(<CandidateScreen navigation={navigation} route={route} />);
+    const accountDetails = (await findAllByTestId('accountsDetails')) as ReactTestInstance[];
     expect(accountDetails.length).toBe(1);
     fireEvent.press(accountDetails[0] as ReactTestInstance);
-    expect(navigateSpy).toBeCalled();
+    expect(navigateSpy).toHaveBeenCalledWith(accountScreen, {
+      address: '13RDY9nrJpyTDBSUdBw12dGwhk19sGwsrVZ2bxkzYHBSagP2',
+    });
   });
 });
