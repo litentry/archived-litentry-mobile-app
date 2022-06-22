382c38903cff48729272195442d62ad338434418
Francisco Silva <franjs.francisco@gmail.com>
2022-06-22 08:34:58 +0200

    adding additional test case

--- src/ui/components/SendFund.test.tsx
+++ src/ui/components/SendFund.test.tsx
@@ -21,7 +21,11 @@ describe('SendFund', () => {
     token: 'DOT',
   } as SubstrateChainRegistry;
 
-  test('component initial state', async () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should render the component with initial state correctly', async () => {
     const {queryByText, getByTestId, findByText} = render(<SendFund address={address} onFundsSent={onFundsSent} />);
 
     await findByText('Enter amount');
@@ -34,7 +38,7 @@ describe('SendFund', () => {
     expect(makeTransferButton).toBeDisabled();
   });
 
-  test.only('make transfer with keep alive check enabled', async () => {
+  it('should make the transaction with `balances.transfer` method when keep alive check is enabled', async () => {
     const {getByPlaceholderText, getByTestId, getByA11yRole, queryByA11yRole} = render(
       <SendFund address={address} onFundsSent={onFundsSent} />,
     );
@@ -71,7 +75,7 @@ describe('SendFund', () => {
     expect(onFundsSent).toHaveBeenCalled();
   });
 
-  test('make transfer with keep alive check disabled', async () => {
+  it('should make the transaction with `balances.transfer` method when keep alive check is disabled', async () => {
     const {getByPlaceholderText, getByTestId, getByText, getByA11yRole, queryByA11yRole} = render(
       <SendFund address={address} onFundsSent={onFundsSent} />,
     );
@@ -109,5 +113,44 @@ describe('SendFund', () => {
     });
 
     expect(onFundsSent).toHaveBeenCalled();
+  });
+
+  it('should not call onFundsSent when the transaction fails', async () => {
+    mockStartTx.mockImplementation(() => Promise.reject());
+
+    const {getByPlaceholderText, getByTestId, getByA11yRole, queryByA11yRole} = render(
+      <SendFund address={address} onFundsSent={onFundsSent} />,
+    );
+
+    const amount = '100';
+    const toAddress = '12NLgzqfhuJkc9mZ5XUTTG85N8yhhzfptwqF1xVhtK3ZX7f6';
+    const _amountBN = stringToBnUtil(mockChainRegistry, amount);
+
+    fireEvent.changeText(getByPlaceholderText('Enter amount'), amount);
+    fireEvent.changeText(getByPlaceholderText('Account address'), toAddress);
+
+    const makeTransferButton = getByTestId('make-transfer-button');
+
+    await waitFor(() => {
+      expect(makeTransferButton).not.toBeDisabled();
+    });
+
+    fireEvent.press(makeTransferButton);
+
+    await waitFor(() => {
+      expect(getByA11yRole('progressbar')).toBeTruthy();
+    });
+
+    expect(mockStartTx).toHaveBeenCalledWith({
+      address,
+      params: [toAddress, _amountBN],
+      txMethod: 'balances.transferKeepAlive',
+    });
+
+    await waitFor(() => {
+      expect(queryByA11yRole('progressbar')).toBeNull();
+    });
+
+    expect(onFundsSent).not.toHaveBeenCalled();
   });
 });
