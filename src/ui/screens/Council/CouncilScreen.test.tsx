ebdf65e4d95f017f2fcca3033fc4bd0882eba3d5
Abhishek <abhishek@litentry.com>
2022-06-22 16:48:40 +0200

    CouncilOverviewScreen testcases updated

--- src/ui/screens/Council/CouncilScreen.test.tsx
+++ src/ui/screens/Council/CouncilScreen.test.tsx
@@ -10,28 +10,64 @@ const navigation = {
   setOptions: () => jest.fn(),
 } as unknown as NavigationProp<DashboardStackParamList>;
 
-test('should render loading state when fetching data', () => {
-  const {getByTestId} = render(<CouncilOverviewScreen navigation={navigation} />);
-  expect(getByTestId('loading_view')).toBeTruthy();
-});
-
-test('should render component when data is fetched', async () => {
-  const {getByText} = render(<CouncilOverviewScreen navigation={navigation} />);
-  await waitFor(() => {
-    expect(getByText('Submit candidacy')).toBeTruthy();
-    expect(getByText('Vote')).toBeTruthy();
-    expect(getByText('Runners Up 17/20')).toBeTruthy();
-    expect(getByText('Prime voter')).toBeTruthy();
-    expect(getByText('RTTI-5220 (POLKADOT)')).toBeTruthy();
+describe('CouncilOverviewScreen', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
   });
-});
 
-test('navigation test case', async () => {
-  const navigateSpy = jest.spyOn(navigation, 'navigate');
-  const {getByText} = render(<CouncilOverviewScreen navigation={navigation} />);
-  await waitFor(() => {
-    expect(getByText('RTTI-5220 (POLKADOT)')).toBeTruthy();
-    fireEvent.press(getByText('RTTI-5220 (POLKADOT)'));
-    expect(navigateSpy).toBeCalledTimes(1);
+  it('should render the loading screen when the data is fetching', () => {
+    const {getByTestId} = render(<CouncilOverviewScreen navigation={navigation} />);
+    expect(getByTestId('loading_view')).toBeTruthy();
+  });
+
+  it('should render the component after data is fetched with initial states ', async () => {
+    const {findByText} = render(<CouncilOverviewScreen navigation={navigation} />);
+    await findByText('Prime voter');
+    await findByText('Vote');
+    await findByText('Submit candidacy');
+    await findByText('Runners Up 17/20');
+  });
+
+  it('should display vote model on clicked on vote button and click cancel button in the model', async () => {
+    const {findByTestId, queryByText, getByTestId} = render(<CouncilOverviewScreen navigation={navigation} />);
+    const voteButton = await findByTestId('vote-button');
+    expect(voteButton).toBeEnabled();
+    expect(queryByText('Vote for council')).toBeNull();
+
+    fireEvent.press(voteButton);
+
+    await waitFor(async () => {
+      expect(queryByText('Vote for council')).toBeDefined();
+      expect(queryByText('voting account:')).toBeDefined();
+      expect(queryByText('Vote value:')).toBeDefined();
+      expect(queryByText('Select up to 16 candidates in the preferred order:')).toBeDefined();
+      const voteCancelButton = getByTestId('vote-cancel-button');
+      fireEvent.press(voteCancelButton);
+    });
+
+    await waitFor(() => {
+      expect(queryByText('Vote for council')).toBeNull();
+    });
+  });
+
+  it('should display submit candidacy model on clicked on submit candidacy button and click cancel button in the model', async () => {
+    const {findByTestId, queryByText, getByTestId} = render(<CouncilOverviewScreen navigation={navigation} />);
+    const submitCandidacyButton = await findByTestId('submit-candidacy-button');
+    expect(submitCandidacyButton).toBeEnabled();
+    expect(queryByText('Submit Council Candidacy')).toBeNull();
+
+    fireEvent.press(submitCandidacyButton);
+
+    await waitFor(async () => {
+      expect(queryByText('Submit Council Candidacy')).toBeDefined();
+      expect(queryByText('Candidate account:')).toBeDefined();
+      expect(queryByText('Candidacy bond:')).toBeDefined();
+      const submitCancelButton = getByTestId('submit-cancel-button');
+      fireEvent.press(submitCancelButton);
+    });
+
+    await waitFor(() => {
+      expect(queryByText('Submit Council Candidacy')).toBeNull();
+    });
   });
 });
