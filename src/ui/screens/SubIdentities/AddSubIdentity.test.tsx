131147068c54ce25f3bb1933fb512c58433c43e6
Abhishek <abhishek@litentry.com>
2022-06-25 20:30:59 +0200

    AddSubIdentity testcases updated

--- src/ui/screens/SubIdentities/AddSubIdentity.test.tsx
+++ src/ui/screens/SubIdentities/AddSubIdentity.test.tsx
@@ -4,35 +4,56 @@ import {AddSubIdentity} from './AddSubIdentity';
 
 jest.useFakeTimers();
 
-const close = () => jest.fn();
+const bottomSheetModal = {
+  dismiss: jest.fn(),
+};
 
-const addSubIdentity = () => jest.fn();
+const addSubIdentity = {
+  subIdentity: jest.fn(),
+};
 
-test('render the AddSubIdentity component', () => {
-  const {getByText, getByPlaceholderText} = render(<AddSubIdentity onClose={close} onAddPress={addSubIdentity} />);
-  expect(getByPlaceholderText('Sub account name (optional)')).toBeTruthy();
-  expect(getByPlaceholderText('👉 Paste address here, e.g. 167r...14h')).toBeTruthy();
-  expect(getByText('Cancel')).toBeTruthy();
-  expect(getByText('Add Identity')).toBeTruthy();
-  expect(getByText(/Add Identity/i) as unknown as HTMLElement).toBeDisabled();
-});
+describe('', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
 
-// TODO: bottom sheets mock
+  it('should render the AddSubIdentity component', async () => {
+    const {findByText, findByPlaceholderText} = render(
+      <AddSubIdentity onClose={bottomSheetModal.dismiss} onAddPress={addSubIdentity.subIdentity} />,
+    );
+    await findByPlaceholderText('Sub account name (optional)');
+    await findByPlaceholderText('👉 Paste address here, e.g. 167r...14h');
+    await findByText('Cancel');
+    await findByText('Add Identity');
+  });
 
-test('Cancel button pressed', () => {
-  // const bottomSheetsSpy = jest.spyOn(useBottomSheetModal, 'dismiss');
-  const {getByText} = render(<AddSubIdentity onClose={close} onAddPress={addSubIdentity} />);
-  expect(getByText('Cancel')).toBeTruthy();
-  fireEvent.press(getByText('Cancel'));
-  // expect(bottomSheetsSpy).toBeCalled()
-});
+  it('should close the bottom-sheets when pressed on cancel button', async () => {
+    const bottomSheetsSpy = jest.spyOn(bottomSheetModal, 'dismiss');
+    const {findByText, findByTestId} = render(
+      <AddSubIdentity onClose={bottomSheetModal.dismiss} onAddPress={addSubIdentity.subIdentity} />,
+    );
+    await findByText('Cancel');
+    fireEvent.press(await findByTestId('cancel-identity-button'));
+    expect(bottomSheetsSpy).toBeCalledTimes(1);
+  });
 
-test('add new identity', () => {
-  const {getByText, getByPlaceholderText} = render(<AddSubIdentity onClose={close} onAddPress={addSubIdentity} />);
-  fireEvent.changeText(
-    getByPlaceholderText('👉 Paste address here, e.g. 167r...14h'),
-    '14yx4vPAACZRhoDQm1dyvXD3QdRQyCRRCe5tj1zPomhhS29a',
-  );
-  expect(getByText(/Add Identity/i) as unknown as HTMLElement).toBeEnabled();
-  fireEvent.press(getByText('Add Identity'));
+  it('should add new sub-identity', async () => {
+    const addSubIdentitySpy = jest.spyOn(addSubIdentity, 'subIdentity');
+    const {getByText, findByPlaceholderText, findByTestId} = render(
+      <AddSubIdentity onClose={bottomSheetModal.dismiss} onAddPress={addSubIdentity.subIdentity} />,
+    );
+    const addIdentityButton = await findByTestId('add-identity-button');
+    expect(addIdentityButton).toBeDisabled();
+    fireEvent.changeText(await findByPlaceholderText('Sub account name (optional)'), 'Sub-account');
+    fireEvent.changeText(
+      await findByPlaceholderText('👉 Paste address here, e.g. 167r...14h'),
+      '14yx4vPAACZRhoDQm1dyvXD3QdRQyCRRCe5tj1zPomhhS29a',
+    );
+    expect(addIdentityButton).toBeEnabled();
+    fireEvent.press(getByText('Add Identity'));
+    expect(addSubIdentitySpy).toBeCalledWith({
+      address: '14yx4vPAACZRhoDQm1dyvXD3QdRQyCRRCe5tj1zPomhhS29a',
+      display: 'Sub-account',
+    });
+  });
 });
