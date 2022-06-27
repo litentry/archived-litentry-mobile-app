1a4a9b1b827f1ab21578fd2c327ad42629b92d70
Abhishek <abhishek@litentry.com>
2022-06-27 14:25:55 +0200

    TreasurySummaryTeaser-pr-fix

--- src/ui/components/TreasurySummaryTeaser.test.tsx
+++ src/ui/components/TreasurySummaryTeaser.test.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import {render, waitFor} from 'src/testUtils';
+import {fireEvent, render} from 'src/testUtils';
 import {TreasurySummaryTeaser} from './TreasurySummaryTeaser';
 
-const onPressEvent = jest.fn();
+const onPressEvent = {
+  onPress: jest.fn(),
+};
 
-test('render the loading view while data is fetching', () => {
-  const {getByTestId} = render(<TreasurySummaryTeaser onPress={onPressEvent} />);
-  expect(getByTestId('loading_box')).toBeTruthy();
-});
+describe('TreasurySummaryTeaser', () => {
+  it('render the loading view while data is fetching', () => {
+    const {getByTestId} = render(<TreasurySummaryTeaser onPress={onPressEvent.onPress} />);
+    expect(getByTestId('loading_box')).toBeTruthy();
+  });
 
-test('test onPress prop functionality', async () => {
-  const {getByText} = render(<TreasurySummaryTeaser onPress={onPressEvent} />);
-  await waitFor(() => {
-    expect(getByText('Proposals')).toBeTruthy();
-    expect(getByText('Totals')).toBeTruthy();
-    expect(getByText('Approved')).toBeTruthy();
-    expect(getByText('Spend period (6 days)')).toBeTruthy();
-    expect(getByText('Available')).toBeTruthy();
-    expect(getByText('Next Burn')).toBeTruthy();
+  it('should render the TreasurySummaryTeaser component when data is fetched', async () => {
+    const {findByText} = render(<TreasurySummaryTeaser onPress={onPressEvent.onPress} />);
+    await findByText('Proposals');
+    await findByText('Totals');
+    await findByText('Approved');
+    await findByText('Spend period (6 days)');
+    await findByText('Available');
+    await findByText('Next Burn');
+  });
+
+  it('should test for onPress props when tapped on BountySummaryTeaser component', async () => {
+    const bountiesTeaserSpy = jest.spyOn(onPressEvent, 'onPress');
+    const {findByText} = render(<TreasurySummaryTeaser onPress={onPressEvent.onPress} />);
+    const bountiesTeaser = await findByText('Proposals');
+    fireEvent.press(bountiesTeaser);
+    expect(bountiesTeaserSpy).toBeCalledTimes(1);
   });
 });
