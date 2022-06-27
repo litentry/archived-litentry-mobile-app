1d9117133fdf53344ee9a427b446cf79da61cf8a
Abhishek <abhishek@litentry.com>
2022-06-27 18:53:53 +0200

    DrawerScreen testcase updated

--- src/ui/screens/Drawer/DrawerScreen.test.tsx
+++ src/ui/screens/Drawer/DrawerScreen.test.tsx
@@ -15,144 +15,65 @@ const state = {
 
 const descriptors = {} as DrawerDescriptorMap;
 
-test('render DrawerScreen', () => {
-  const {getByText} = render(<DrawerScreen navigation={navigation} state={state} descriptors={descriptors} />);
-  expect(getByText('Dashboard')).toBeTruthy();
-  expect(getByText('Registrars')).toBeTruthy();
-  expect(getByText('Tech. Committee')).toBeTruthy();
-  expect(getByText('Discussions')).toBeTruthy();
-  expect(getByText('Accounts')).toBeTruthy();
-
-  expect(getByText('Parachains')).toBeTruthy();
-  expect(getByText('Overview')).toBeTruthy();
-  expect(getByText('Parathreads')).toBeTruthy();
-  expect(getByText('Auctions')).toBeTruthy();
-  expect(getByText('Crowdloan')).toBeTruthy();
-
-  expect(getByText('Settings')).toBeTruthy();
-  expect(getByText('Dark theme')).toBeTruthy();
-  expect(getByText('Notifications')).toBeTruthy();
-  expect(getByText('Dev Kit')).toBeTruthy();
-
-  expect(getByText('About Litentry')).toBeTruthy();
-  expect(getByText('Feedback')).toBeTruthy();
-});
-
-test('drawerScreen and navigation case Dashboard', () => {
-  const navigationSpy = jest.spyOn(navigation, 'navigate');
-  const {getByText} = render(<DrawerScreen navigation={navigation} state={state} descriptors={descriptors} />);
-
-  expect(getByText('Dashboard')).toBeTruthy();
-  fireEvent.press(getByText('Dashboard'));
-  expect(navigationSpy).toBeCalledWith('Dashboard', undefined);
-});
-
-test('drawerScreen and navigation case Registrars', () => {
-  const navigationSpy = jest.spyOn(navigation, 'navigate');
-  const {getByText} = render(<DrawerScreen navigation={navigation} state={state} descriptors={descriptors} />);
-
-  expect(getByText('Registrars')).toBeTruthy();
-  fireEvent.press(getByText('Registrars'));
-  expect(navigationSpy).toBeCalledWith('Registrars', undefined);
-});
-
-test('drawerScreen and navigation case technical committee', () => {
-  const navigationSpy = jest.spyOn(navigation, 'navigate');
-  const {getByText} = render(<DrawerScreen navigation={navigation} state={state} descriptors={descriptors} />);
-
-  expect(getByText('Tech. Committee')).toBeTruthy();
-  fireEvent.press(getByText('Tech. Committee'));
-  expect(navigationSpy).toBeCalledWith('Technical Committee', undefined);
-});
-
-test('drawerScreen and navigation case Discussions', () => {
-  const navigationSpy = jest.spyOn(navigation, 'navigate');
-  const {getByText} = render(<DrawerScreen navigation={navigation} state={state} descriptors={descriptors} />);
-
-  expect(getByText('Discussions')).toBeTruthy();
-  fireEvent.press(getByText('Discussions'));
-  expect(navigationSpy).toBeCalledWith('PolkassemblyDiscussionsNavigator', undefined);
-});
-
-test('drawerScreen and navigation case Accounts', () => {
-  const navigationSpy = jest.spyOn(navigation, 'navigate');
-  const {getByText} = render(<DrawerScreen navigation={navigation} state={state} descriptors={descriptors} />);
-
-  expect(getByText('Accounts')).toBeTruthy();
-  fireEvent.press(getByText('Accounts'));
-  expect(navigationSpy).toBeCalledWith('AccountsNavigator', undefined);
-});
-test('drawerScreen and navigation case Overview', () => {
-  const navigationSpy = jest.spyOn(navigation, 'navigate');
-  const {getByText} = render(<DrawerScreen navigation={navigation} state={state} descriptors={descriptors} />);
-
-  expect(getByText('Overview')).toBeTruthy();
-  fireEvent.press(getByText('Overview'));
-  expect(navigationSpy).toBeCalledWith('ParachainsNavigator', undefined);
-});
-
-test('drawerScreen and navigation case Parathreads', () => {
-  const navigationSpy = jest.spyOn(navigation, 'navigate');
-  const {getByText} = render(<DrawerScreen navigation={navigation} state={state} descriptors={descriptors} />);
-
-  expect(getByText('Parathreads')).toBeTruthy();
-  fireEvent.press(getByText('Parathreads'));
-  expect(navigationSpy).toBeCalledWith('ParachainsNavigator', undefined);
-});
-
-test('drawerScreen and navigation case Auctions', () => {
-  const navigationSpy = jest.spyOn(navigation, 'navigate');
-  const {getByText} = render(<DrawerScreen navigation={navigation} state={state} descriptors={descriptors} />);
-
-  expect(getByText('Auctions')).toBeTruthy();
-  fireEvent.press(getByText('Auctions'));
-  expect(navigationSpy).toBeCalledWith('Auctions', undefined);
-});
-
-test('drawerScreen and navigation case Crowdloan', () => {
-  const navigationSpy = jest.spyOn(navigation, 'navigate');
-  const {getByText} = render(<DrawerScreen navigation={navigation} state={state} descriptors={descriptors} />);
-
-  expect(getByText('Crowdloan')).toBeTruthy();
-  fireEvent.press(getByText('Crowdloan'));
-  expect(navigationSpy).toBeCalledWith('CrowdloansNavigator', undefined);
-});
-
-test('drawerScreen and navigation case notifications', () => {
-  const navigationSpy = jest.spyOn(navigation, 'navigate');
-  const {getByText} = render(<DrawerScreen navigation={navigation} state={state} descriptors={descriptors} />);
-
-  expect(getByText('Notifications')).toBeTruthy();
-  fireEvent.press(getByText('Notifications'));
-  expect(navigationSpy).toBeCalledWith('Notification', undefined);
-});
-
-test('drawerScreen and navigation case dev-kit', () => {
-  const navigationSpy = jest.spyOn(navigation, 'navigate');
-  const {getByText} = render(<DrawerScreen navigation={navigation} state={state} descriptors={descriptors} />);
-
-  expect(getByText('Dev Kit')).toBeTruthy();
-  fireEvent.press(getByText('Dev Kit'));
-  expect(navigationSpy).toBeCalledWith('Dev Kit', undefined);
-});
-
-test('drawerScreen and navigation case litentry', () => {
-  const navigationSpy = jest.spyOn(navigation, 'navigate');
-  const {getByText} = render(<DrawerScreen navigation={navigation} state={state} descriptors={descriptors} />);
-
-  expect(getByText('About Litentry')).toBeTruthy();
-  fireEvent.press(getByText('About Litentry'));
-  expect(navigationSpy).toBeCalledWith('Webview', {
-    title: 'About Litentry',
-    uri: 'https://www.litentry.com',
+describe('DrawerScreen', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
   });
-});
+  it('should render DrawerScreen with all the side navigation items', async () => {
+    const {findByText} = render(<DrawerScreen navigation={navigation} state={state} descriptors={descriptors} />);
+    await findByText('Dashboard');
+    await findByText('Registrars');
+    await findByText('Tech. Committee');
+    await findByText('Discussions');
+    await findByText('Accounts');
 
-test('drawerScreen and navigation case feedback', () => {
-  const navigationSpy = jest.spyOn(navigation, 'navigate');
-  const {getByText} = render(<DrawerScreen navigation={navigation} state={state} descriptors={descriptors} />);
+    await findByText('Parachains');
+    await findByText('Overview');
+    await findByText('Parathreads');
+    await findByText('Auctions');
+    await findByText('Crowdloan');
 
-  expect(getByText('Feedback')).toBeTruthy();
-  fireEvent.press(getByText('Feedback'));
-  expect(navigationSpy).toBeCalledWith('Feedback', undefined);
+    await findByText('Settings');
+    await findByText('Dark theme');
+    await findByText('Notifications');
+    await findByText('Dev Kit');
+
+    await findByText('About Litentry');
+    await findByText('Feedback');
+  });
+
+  const drawerItems = [
+    ['Dashboard', 'Dashboard'],
+    ['Registrars', 'Registrars'],
+    ['Tech. Committee', 'Technical Committee'],
+    ['Discussions', 'PolkassemblyDiscussionsNavigator'],
+    ['Accounts', 'AccountsNavigator'],
+    ['Overview', 'ParachainsNavigator'],
+    ['Parathreads', 'Parathreads'],
+    ['Auctions', 'Auctions'],
+    ['Crowdloan', 'CrowdloansNavigator'],
+    ['Notifications', 'Notification'],
+    ['Dev Kit', 'Dev Kit'],
+    ['Crowdloan', 'CrowdloansNavigator'],
+    ['Feedback', 'Feedback'],
+  ];
+
+  it.each(drawerItems)('should navigate to %s screen when pressed on %s item', async (item, routeKey) => {
+    const navigationSpy = jest.spyOn(navigation, 'navigate');
+    const {findByText} = render(<DrawerScreen navigation={navigation} state={state} descriptors={descriptors} />);
+    fireEvent.press(await findByText(item as string));
+    expect(navigationSpy).toBeCalledWith(routeKey, undefined);
+  });
+
+  it('should navigate to Webview when pressed on About Litentry', () => {
+    const navigationSpy = jest.spyOn(navigation, 'navigate');
+    const {getByText} = render(<DrawerScreen navigation={navigation} state={state} descriptors={descriptors} />);
+
+    expect(getByText('About Litentry')).toBeTruthy();
+    fireEvent.press(getByText('About Litentry'));
+    expect(navigationSpy).toBeCalledWith('Webview', {
+      title: 'About Litentry',
+      uri: 'https://www.litentry.com',
+    });
+  });
 });
