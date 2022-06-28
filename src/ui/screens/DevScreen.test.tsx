d31ec36c914e629d3ec2b39b164a5804658e2c1e
Abhishek <abhishek@litentry.com>
2022-06-28 21:31:15 +0200

    DevScreen testcase updated

--- src/ui/screens/DevScreen.test.tsx
+++ src/ui/screens/DevScreen.test.tsx
@@ -1,27 +1,28 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {Notification} from 'react-native-in-app-message';
 import {ReactTestInstance} from 'react-test-renderer';
 import {fireEvent, render} from 'src/testUtils';
 import DevScreen from './DevScreen';
 
-jest.useFakeTimers();
+describe('DevScreen', () => {
+  it('should render the DevScreen component', async () => {
+    const {findByText, findAllByText} = render(<DevScreen />);
+    await findByText('Network: Polkadot');
+    await findByText('wss://rpc.polkadot.io');
+    await findByText('Active account:');
+    await findByText('Simple Notification');
+    await findByText('Show simple text in app PN');
+    await findAllByText('Trigger');
+    await findByText('Show multi-lines Notification');
+    await findByText('Show multi-lines In-App-PusNotification');
+  });
 
-test('render the DevScreen component', () => {
-  const {getByText, getAllByText} = render(<DevScreen />);
-  expect(getByText('Network: Polkadot')).toBeTruthy();
-  expect(getByText('wss://rpc.polkadot.io')).toBeTruthy();
-  expect(getByText('Active account:')).toBeTruthy();
-  expect(getByText('Simple Notification')).toBeTruthy();
-  expect(getByText('Show simple text in app PN')).toBeTruthy();
-  expect(getAllByText('Trigger').length).toBe(2);
-  expect(getByText('Show multi-lines Notification')).toBeTruthy();
-  expect(getByText('Show multi-lines In-App-PusNotification')).toBeTruthy();
-});
-
-test('test sample notification', () => {
-  // const notificationSpy = jest.spyOn(Notification, "show")
-  const {getAllByText} = render(<DevScreen />);
-  expect(getAllByText('Trigger').length).toBe(2);
-  fireEvent.press(getAllByText('Trigger')[0] as ReactTestInstance);
-  fireEvent.press(getAllByText('Trigger')[1] as ReactTestInstance);
+  it('should test sample notification', () => {
+    const notificationRef = useRef<Notification>(null).current as any;
+    const notificationSpy = jest.spyOn(notificationRef, 'show');
+    const {getAllByText} = render(<DevScreen />);
+    expect(getAllByText('Trigger').length).toBe(2);
+    fireEvent.press(getAllByText('Trigger')[0] as ReactTestInstance);
+    expect(notificationSpy).toBeCalledTimes(1);
+  });
 });
