32f9217ae2b38d8029d64e82d5fd59538a7da562
Ishrak Imam <ishrak@litentry.com>
2022-06-29 17:32:54 +0200

    Updating logic for push notificaton subscribe in android

--- src/hooks/useTurnOnAllNotificationsOnAppStartForAndroid.ts
+++ src/hooks/useTurnOnAllNotificationsOnAppStartForAndroid.ts
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Platform} from 'react-native';
-import {usePushTopics} from './usePushTopics';
+import {usePushTopics} from '@atoms/pushNotification';
 import {isFirstAppStart} from 'src/utils/firstAppStart';
 
 export function useTurnOnAllNotificationsOnAppStartForAndroid() {
-  const {subscribeToAllTopics, isLoading: topicsLoading} = usePushTopics();
+  const {subscribeToAllTopics} = usePushTopics();
 
   const isRunnedOnceRef = React.useRef(false);
-  const shouldToggle = !topicsLoading && isFirstAppStart && Platform.OS === 'android' && !isRunnedOnceRef.current;
+  const shouldToggle = isFirstAppStart && Platform.OS === 'android' && !isRunnedOnceRef.current;
 
   // toggle all topics to true on first app start
   // on IOS we will do that after push permission granted
