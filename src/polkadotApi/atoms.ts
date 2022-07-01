da77b5ac2f3732ff095ce03b80d909c3bc6bcaa1
Francisco Silva <franjs.francisco@gmail.com>
2022-07-01 12:28:59 +0200

    updating polkadotApi atoms

--- src/polkadotApi/atoms.ts
+++ src/polkadotApi/atoms.ts
@@ -1,17 +1,26 @@
 import {atom, RecoilState} from 'recoil';
-import type {
-  MnemonicLength,
-  AddAccountPayload,
-  AddExternalAccountPayload,
-  RestoreAccountPayload,
-  ExportAccountPayload,
-  Accounts,
-  SignCredentials,
-  SignedMessage,
-} from './types';
 import {persistAtom} from '@atoms/persist';
+import {
+  KeyringAccount,
+  AddAccountMessage,
+  AddExternalAccountMessage,
+  CreateAddressFromMnemonicMessage,
+  CreateAddressFromMnemonicResultMessage,
+  ExportAccountMessage,
+  ForgetAccountMessage,
+  GenerateMnemonicMessage,
+  GenerateMnemonicResultMessage,
+  RestoreAccountMessage,
+  SignMessage,
+  UpdateAccountMetaMessage,
+  ValidateMnemonicMessage,
+  ValidateMnemonicResultMessage,
+  VerifyCredentialsMessage,
+  VerifyCredentialsResultMessage,
+  HexString,
+} from 'polkadot-api';
 
-export const appAccountsState: RecoilState<Accounts> = atom({
+export const appAccountsState: RecoilState<Record<string, KeyringAccount>> = atom({
   key: 'appAccounts',
   default: {},
   effects: [persistAtom],
@@ -20,41 +29,71 @@ export const appAccountsState: RecoilState<Accounts> = atom({
 export const cryptoUtilState = atom({
   key: 'cryptoUtil',
   default: {
-    generateMnemonic: (_?: MnemonicLength) => Promise.resolve(''),
-    verifyMnemonic: (_: string) => Promise.resolve({isValid: false, address: undefined}),
+    generateMnemonic: (_?: GenerateMnemonicMessage['payload']) => {
+      return Promise.resolve<GenerateMnemonicResultMessage['payload']>({mnemonic: ''});
+    },
+    validateMnemonic: (_: ValidateMnemonicMessage['payload']) => {
+      return Promise.resolve<ValidateMnemonicResultMessage['payload']>({isValid: false, address: ''});
+    },
   },
 });
+
+const emptyAccountJson: KeyringAccount = {
+  address: '',
+  meta: {
+    name: '',
+    network: '',
+    isFavorite: false,
+    isExternal: false,
+  },
+  encoded: '',
+  encoding: {content: [''], type: 'none', version: '0'},
+};
 
 export const keyringState = atom({
   key: 'keyring',
   default: {
-    createAccount: (_: string) => Promise.resolve(''),
-    addAccount: (_: AddAccountPayload) => Promise.resolve<Record<string, unknown>>({}),
-    addExternalAccount: (_: AddExternalAccountPayload) => Promise.resolve<Record<string, unknown>>({}),
-    forgetAccount: (_: string) => {
+    createAddressFromMnemonic: (_: CreateAddressFromMnemonicMessage['payload']) => {
+      return Promise.resolve<CreateAddressFromMnemonicResultMessage['payload']>({address: ''});
+    },
+    addAccount: (_: AddAccountMessage['payload']) => {
+      return Promise.resolve<KeyringAccount>(emptyAccountJson);
+    },
+    addExternalAccount: (_: AddExternalAccountMessage['payload']) => {
+      return Promise.resolve<KeyringAccount>(emptyAccountJson);
+    },
+    forgetAccount: (_: ForgetAccountMessage['payload']) => {
       return;
     },
-    updateMeta: (_: string, __: Record<string, unknown>) => {
+    updateAccountMeta: (_: UpdateAccountMetaMessage['payload']) => {
       return;
     },
-    restoreAccount: (_: RestoreAccountPayload) => Promise.resolve<Record<string, unknown>>({}),
-    exportAccount: (_: ExportAccountPayload) => Promise.resolve<Record<string, unknown>>({}),
-    sign: (_: string, __: SignCredentials) => Promise.resolve<SignedMessage>({signed: '0x...'}),
-    verifyCredentials: (_: SignCredentials) => Promise.resolve({valid: false}),
+    restoreAccount: (_: RestoreAccountMessage['payload']) => {
+      return Promise.resolve<KeyringAccount>(emptyAccountJson);
+    },
+    exportAccount: (_: ExportAccountMessage['payload']) => {
+      return Promise.resolve<KeyringAccount>(emptyAccountJson);
+    },
+    sign: (_: SignMessage['payload']) => {
+      return Promise.resolve<HexString>('0x00000');
+    },
+    verifyCredentials: (_: VerifyCredentialsMessage['payload']) => {
+      return Promise.resolve<VerifyCredentialsResultMessage['payload']>({valid: false});
+    },
   },
 });
 
-// export const apiState = atom({
-//   key: 'polkadot-api',
-//   default: {
-//     isConnecting: false,
-//     isReady: false,
-//   },
-// });
+export const apiState = atom({
+  key: 'polkadot-api',
+  default: {
+    isConnecting: false,
+    isReady: false,
+  },
+});
 
-// export const txState = atom({
-//   key: 'tx',
-//   default: {
-//     getChainName: () => Promise.resolve(''),
-//   },
-// });
+export const txState = atom({
+  key: 'tx',
+  default: {
+    getChainName: () => Promise.resolve(''),
+  },
+});
