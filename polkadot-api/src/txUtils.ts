c55a79997aa1ba77d8c92fbfc187dd8754e3c082
Francisco Silva <franjs.francisco@gmail.com>
2022-07-05 17:00:13 +0200

    adding tx utils to get txInfo and send transactions

---
+++ polkadot-api/src/txUtils.ts
@@ -0,0 +1,180 @@
+import {ApiPromise} from '@polkadot/api';
+import type {SubmittableExtrinsic, SignerOptions} from '@polkadot/api/submittable/types';
+import type {
+  FunctionMetadataLatest,
+  Hash,
+  ExtrinsicEra,
+  Index,
+  Header,
+  SignerPayload,
+  ExtrinsicPayload,
+} from '@polkadot/types/interfaces';
+import type {SignerPayloadJSON, SignatureOptions} from '@polkadot/types/types';
+import type {Registry} from '@polkadot/types-codec/types';
+import {BN_ZERO, assert, isNumber, isUndefined, objectSpread} from '@polkadot/util';
+import type {TxConfig} from './txTypes';
+import {HexString} from './messages';
+
+export type TxInfo = {
+  title: string;
+  description: string;
+  partialFee: number;
+  txPayload: SignerPayloadJSON;
+  signablePayload: Uint8Array;
+};
+
+const signerOptions: Partial<SignerOptions> = {
+  nonce: -1,
+  tip: BN_ZERO,
+};
+
+export async function getTxInfo(api: ApiPromise, address: string, txConfig: TxConfig): Promise<TxInfo> {
+  const transaction = buildTransaction(api, txConfig);
+  const {meta} = transaction.registry.findMetaCall(transaction.callIndex);
+  const args = meta?.args.map(({name}) => name).join(', ') || '';
+  const title = `transaction ${txConfig.method}(${args})`;
+  const description = formatCallMeta(meta);
+  const info = await transaction.paymentInfo(address);
+  const partialFee = info.partialFee.toNumber();
+  const signingInfo = await api.derive.tx.signingInfo(address, signerOptions.nonce);
+  const eraOptions = makeEraOptions(api, transaction.registry, signerOptions, signingInfo);
+  const txPayload = transaction.registry
+    .createTypeUnsafe<SignerPayload>('SignerPayload', [
+      objectSpread({}, eraOptions, {
+        address,
+        blockNumber: signingInfo.header ? signingInfo.header.number : 0,
+        method: transaction.method,
+      }),
+    ])
+    .toPayload();
+  const extrinsicPayload: ExtrinsicPayload = transaction.registry.createType('ExtrinsicPayload', txPayload, {
+    version: txPayload.version,
+  });
+
+  return {
+    title,
+    description,
+    partialFee,
+    txPayload,
+    signablePayload: extrinsicPayload.toU8a({method: true}),
+  };
+}
+
+export async function sendTx(
+  api: ApiPromise,
+  address: string,
+  txConfig: TxConfig,
+  txPayload: SignerPayloadJSON,
+  signature: HexString,
+): Promise<void> {
+  const transaction = buildTransaction(api, txConfig);
+  transaction.addSignature(address, signature, txPayload);
+
+  return new Promise((resolve, reject) => {
+    transaction.send((result) => {
+      if (result.isFinalized && !result.isError) {
+        resolve();
+      }
+      if (result.isError && result.dispatchError) {
+        let error;
+        if (result.dispatchError.isModule) {
+          // for module errors, we have the section indexed, lookup
+          const decoded = api.registry.findMetaError(result.dispatchError.asModule);
+          const {docs} = decoded;
+          error = docs.join(' ').trim();
+        } else {
+          error = result.dispatchError.toString();
+        }
+        reject(error);
+      }
+    });
+  });
+}
+
+function buildTransaction(api: ApiPromise, txConfig: TxConfig): SubmittableExtrinsic<'promise'> {
+  const {method, params} = txConfig;
+  const [section, txMethod] = method.split('.');
+
+  if (!section || !txMethod) {
+    throw new Error(`Invalid method ${section}.${txMethod}`);
+  }
+
+  const tx = api.tx[section]?.[txMethod];
+
+  if (typeof tx !== 'function') {
+    throw new Error(`Unable to find method ${section}.${txMethod}`);
+  }
+
+  return tx(...params);
+}
+
+function formatCallMeta(meta?: FunctionMetadataLatest): string {
+  if (!meta || !meta.docs.length) {
+    return '';
+  }
+
+  const strings = meta.docs.map((doc) => doc.toString().trim());
+  const firstEmpty = strings.findIndex((doc) => !doc.length);
+  const combined = (firstEmpty === -1 ? strings : strings.slice(0, firstEmpty))
+    .join(' ')
+    .replace(/#(<weight>| <weight>).*<\/weight>/, '');
+  const parts = splitParts(combined.replace(/\\/g, '').replace(/`/g, ''));
+
+  return parts.join(' ');
+}
+
+function splitParts(value: string): string[] {
+  return ['[', ']'].reduce((result: string[], sep) => splitSingle(result, sep), [value]);
+}
+
+function splitSingle(value: string[], sep: string): string[] {
+  return value.reduce((result: string[], _value: string): string[] => {
+    return _value.split(sep).reduce((_result: string[], __value: string) => _result.concat(__value), result);
+  }, []);
+}
+
+function makeEraOptions(
+  api: ApiPromise,
+  registry: Registry,
+  partialOptions: Partial<SignerOptions>,
+  {header, mortalLength, nonce}: {header: Header | null; mortalLength: number; nonce: Index},
+): SignatureOptions {
+  if (!header) {
+    assert(
+      partialOptions.era === 0 || !isUndefined(partialOptions.blockHash),
+      'Expected blockHash to be passed alongside non-immortal era options',
+    );
+
+    if (isNumber(partialOptions.era)) {
+      // since we have no header, it is immortal, remove any option overrides
+      // so we only supply the genesisHash and no era to the construction
+      delete partialOptions.era;
+      delete partialOptions.blockHash;
+    }
+
+    return makeSignOptions(api, partialOptions, {nonce});
+  }
+
+  return makeSignOptions(api, partialOptions, {
+    blockHash: header.hash,
+    era: registry.createTypeUnsafe<ExtrinsicEra>('ExtrinsicEra', [
+      {
+        current: header.number,
+        period: partialOptions.era || mortalLength,
+      },
+    ]),
+    nonce,
+  });
+}
+
+function makeSignOptions(
+  api: ApiPromise,
+  partialOptions: Partial<SignerOptions>,
+  extras: {blockHash?: Hash; era?: ExtrinsicEra; nonce?: Index},
+): SignatureOptions {
+  return objectSpread({blockHash: api.genesisHash, genesisHash: api.genesisHash}, partialOptions, extras, {
+    runtimeVersion: api.runtimeVersion,
+    signedExtensions: api.registry.signedExtensions,
+    version: api.extrinsicVersion,
+  });
+}
