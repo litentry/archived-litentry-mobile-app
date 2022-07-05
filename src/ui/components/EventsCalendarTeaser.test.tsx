4b7f5932fb1b64526aba441876fa088b502413d6
Abhishek <abhishek@litentry.com>
2022-07-05 10:55:36 +0200

    EventsCalendarTeaser testcase updated

--- src/ui/components/EventsCalendarTeaser.test.tsx
+++ src/ui/components/EventsCalendarTeaser.test.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import {render, waitFor} from 'src/testUtils';
+import {fireEvent, render} from 'src/testUtils';
 import {EventsCalendarTeaser} from './EventsCalendarTeaser';
 
-const onPressEvent = jest.fn();
+const onPressEvent = {
+  pressEvent: jest.fn(),
+};
 
-test('should render the loading view when loading', async () => {
-  const {getByTestId} = render(<EventsCalendarTeaser onPress={onPressEvent} />);
-  expect(getByTestId('loading_box')).toBeTruthy();
-});
+describe('EventsCalendarTeaser', () => {
+  it('should render the loading view when loading', async () => {
+    const {getByTestId} = render(<EventsCalendarTeaser onPress={onPressEvent.pressEvent} />);
+    expect(getByTestId('loading_box')).toBeTruthy();
+  });
 
-test('render when there are events', async () => {
-  const {getByText} = render(<EventsCalendarTeaser onPress={onPressEvent} />);
-  await waitFor(() => {
-    expect(getByText('Start of a new staking session 4,121')).toBeTruthy();
-    expect(getByText('Start of a new staking era 681')).toBeTruthy();
-    expect(getByText('Election of new council candidates')).toBeTruthy();
+  it('should render when there are events', async () => {
+    const {findByText} = render(<EventsCalendarTeaser onPress={onPressEvent.pressEvent} />);
+    await findByText('Start of a new staking session 4,121');
+    await findByText('Start of a new staking era 681');
+    await findByText('Election of new council candidates');
+  });
+
+  it('should navigate to events calender component when pressed on the teaser', async () => {
+    const navigationSpy = jest.spyOn(onPressEvent, 'pressEvent');
+    const {findByText} = render(<EventsCalendarTeaser onPress={onPressEvent.pressEvent} />);
+    fireEvent.press(await findByText('Start of a new staking session 4,121'));
+    expect(navigationSpy).toHaveBeenCalledTimes(1);
   });
 });
