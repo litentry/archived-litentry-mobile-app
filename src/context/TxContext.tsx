05a1adf3c1942877bd7d503ebbd986edc8483a3d
Francisco Silva <franjs.francisco@gmail.com>
2022-07-07 17:27:35 +0200

    refactoring TxContext

--- src/context/TxContext.tsx
+++ src/context/TxContext.tsx
@@ -1,16 +1,7 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import React, {createContext, useCallback, useEffect, useMemo, useReducer, useRef, useContext} from 'react';
+import React, {createContext, useEffect, useCallback, useMemo, useReducer, useContext} from 'react';
 import {Dimensions, StyleSheet} from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
-import {SubmittableExtrinsic} from '@polkadot/api/submittable/types';
-import {SignerPayloadJSON, SignerResult} from '@polkadot/types/types';
-import {ExtrinsicPayload} from '@polkadot/types/interfaces';
-import {BN_ZERO, /* hexToU8a, u8aConcat, */ u8aToHex} from '@polkadot/util';
-import {ApiPromise} from '@polkadot/api';
-import {get} from 'lodash';
-import {formatCallMeta} from 'src/utils/callMetadata';
-import AsyncSigner from 'src/service/AsyncSigner';
-import {useApi} from 'context/ChainApiContext';
+import {u8aToHex} from '@polkadot/util';
 import LoadingView from '@ui/components/LoadingView';
 import {AuthenticateView} from '@ui/components/Tx/AuthenticateView';
 import {PayloadQrCodeView} from '@ui/components/Tx/PayloadQrCodeView';
@@ -21,11 +12,13 @@ import {MessageTeaser} from '@ui/components/MessageTeaser';
 import {Subheading, Caption, Icon, useBottomSheet, Button} from '@ui/library';
 import globalStyles, {standardPadding} from '@ui/styles';
 import {Padder} from '@ui/components/Padder';
-import type {SignCredentials} from 'polkadot-api';
+import type {HexString, SendTxMessage, SignCredentials} from 'polkadot-api';
 import {useKeyring} from '@polkadotApi/useKeyring';
 import {useAppAccounts} from '@polkadotApi/useAppAccounts';
-
-let id = 0;
+import {useTx} from '@polkadotApi/useTx';
+import {TxConfig} from 'polkadot-api/src/txTypes';
+import {TxInfo} from 'polkadot-api/src/txUtils';
+import {usePolkadotApiState} from '@polkadotApi/usePolkadotApiState';
 
 const {width, height} = Dimensions.get('window');
 
@@ -33,146 +26,71 @@ type TxProviderProps = {
   children: React.ReactNode;
 };
 type TxContextValueType = {
-  start: (config: StartConfig) => Promise<void>;
+  startTx: (config: StartConfig) => Promise<void>;
 };
 
 const TxContext = createContext<TxContextValueType>({
-  start: () => Promise.resolve(),
+  startTx: () => Promise.resolve(),
 });
 
 export type StartConfig = {
-  api: ApiPromise;
   address: string;
-  txMethod: string;
-  params: unknown[];
+  txConfig: TxConfig;
 };
 
 export function TxProvider({children}: TxProviderProps): React.ReactElement {
   const {BottomSheet, openBottomSheet, closeBottomSheet} = useBottomSheet();
-  const signTransactionRef = useRef<(value: SignerResult) => void>();
-  const showPreviewRef = useRef<(txPayload: SignerPayloadJSON, credentials?: SignCredentials) => Promise<void>>();
   const [state, dispatch] = useReducer(reducer, initialState);
-  const {api: apiPromise} = useApi();
   const {sign} = useKeyring();
+  const {getTxInfo, sendTx} = useTx();
+  const apiState = usePolkadotApiState();
 
   useEffect(() => {
-    if (!apiPromise?.isConnected && state.view === 'submitting_view') {
+    if (!apiState.isReady) {
       dispatch({
         type: 'SHOW_WARNING',
         payload: 'The transaction was sent but you got disconnected from the chain. Please verify later.',
       });
     }
-  }, [apiPromise?.isConnected, state.view]);
+  }, [apiState.isReady, state.view]);
+
   const {accounts} = useAppAccounts();
 
-  const start = useCallback(
+  const sendTransaction = useCallback(
+    (payload: SendTxMessage['payload']) => {
+      sendTx(payload)
+        .then(() => {
+          dispatch({type: 'SHOW_SUCCESS_VIEW'});
+        })
+        .catch((error) => {
+          dispatch({type: 'SHOW_ERROR', payload: error});
+        });
+    },
+    [sendTx],
+  );
+
+  const startTx = useCallback(
     async (config: StartConfig) => {
-      const {api, txMethod, params, address} = config;
-      const [section, method] = txMethod.split('.');
-
-      if (!section || !method) {
-        throw new Error('section or method undefined');
-      }
-
-      if (!get(api.tx, [section, method])) {
-        throw new Error(`Unable to find method ${section}.${method}`);
-      }
-
-      const transaction: SubmittableExtrinsic<'promise'> = api.tx[section]![method]!(...params);
-
       openBottomSheet();
-
-      const {meta} = transaction.registry.findMetaCall(transaction.callIndex);
-      const args = meta?.args.map(({name}) => name).join(', ') || '';
-      const title = `Sending transaction ${section}.${method}(${args})`;
-      const description = formatCallMeta(meta);
+      const {address, txConfig} = config;
+      const txInfo = await getTxInfo({address, txConfig});
       const isExternal = Boolean(accounts[address]?.meta.isExternal);
 
-      showPreviewRef.current = async (txPayload: SignerPayloadJSON, credentials?: SignCredentials) => {
-        const info = await transaction.paymentInfo(address);
-        if (isExternal) {
-          dispatch({
-            type: 'SHOW_TX_PREVIEW',
-            payload: {
-              txPayload: txPayload,
-              params,
-              title,
-              description,
-              partialFee: info.partialFee.toNumber(),
-              isExternalAccount: true,
-            },
-          });
-        } else {
-          if (!credentials) {
-            throw new Error('Credentials is required for non-external accounts');
-          }
-          const extrinsicPayload: ExtrinsicPayload = transaction.registry.createType('ExtrinsicPayload', txPayload, {
-            version: txPayload.version,
-          });
-
-          const signable = u8aToHex(extrinsicPayload.toU8a({method: true}));
-          const signed = await sign({message: signable, credentials});
-
-          dispatch({
-            type: 'SHOW_TX_PREVIEW',
-            payload: {
-              txPayload: txPayload,
-              params,
-              title,
-              description,
-              partialFee: info.partialFee.toNumber(),
-              isExternalAccount: false,
-              signerResult: {id: ++id, signature: signed},
-            },
-          });
-        }
-      };
-
-      try {
-        const f = await transaction.signAsync(address, {
-          nonce: -1,
-          tip: BN_ZERO,
-          signer: new AsyncSigner(async (txPayload) => {
-            if (!isExternal) {
-              dispatch({type: 'SHOW_AUTHENTICATE_VIEW', payload: {txPayload, address}});
-            } else {
-              showPreviewRef.current?.(txPayload);
-            }
-            return new Promise((resolve) => {
-              signTransactionRef.current = resolve;
-            });
-          }),
+      if (isExternal) {
+        dispatch({
+          type: 'SHOW_TX_PREVIEW',
+          payload: {
+            txInfo,
+            txConfig,
+            address,
+            isExternalAccount: true,
+          },
         });
-
-        await new Promise((resolve, reject) => {
-          f.send((result) => {
-            if (result.isFinalized && !result.isError) {
-              dispatch({type: 'SHOW_SUCCESS_VIEW'});
-              resolve(undefined);
-            }
-            if (result.isError && result.dispatchError) {
-              let error;
-              if (result.dispatchError.isModule) {
-                // for module errors, we have the section indexed, lookup
-                const decoded = api.registry.findMetaError(result.dispatchError.asModule);
-                const {docs} = decoded;
-                error = docs.join(' ').trim();
-              } else {
-                error = result.dispatchError.toString();
-              }
-              dispatch({type: 'SHOW_ERROR', payload: error});
-              reject();
-            }
-          }).catch((error) => {
-            reject(error.message);
-          });
-        });
-      } catch (e) {
-        console.warn('transaction error', e);
-        dispatch({type: 'SHOW_ERROR', payload: String(e)});
+      } else {
+        dispatch({type: 'SHOW_AUTHENTICATE_VIEW', payload: {address, txConfig, txInfo}});
       }
     },
-    [accounts, openBottomSheet, sign],
+    [accounts, openBottomSheet, getTxInfo],
   );
 
   const reset = React.useCallback(() => {
@@ -193,8 +111,16 @@ export function TxProvider({children}: TxProviderProps): React.ReactElement {
         return (
           <AuthenticateView
             address={state.address}
-            onAuthenticate={(credentials) => {
-              showPreviewRef.current?.(state.txPayload, credentials);
+            onAuthenticate={async (credentials) => {
+              dispatch({
+                type: 'SHOW_TX_PREVIEW',
+                payload: {
+                  txConfig: state.txConfig,
+                  txInfo: state.txInfo,
+                  credentials,
+                  isExternalAccount: false,
+                },
+              });
             }}
           />
         );
@@ -202,19 +128,29 @@ export function TxProvider({children}: TxProviderProps): React.ReactElement {
       case 'tx_preview':
         return (
           <TxPreview
-            transactionInfo={state.description}
-            transactionTitle={state.title}
-            txPayload={state.txPayload}
-            params={state.params}
-            partialFee={state.partialFee}
+            transactionInfo={state.txInfo.description}
+            transactionTitle={`Sending ${state.txInfo.title}`}
+            txPayload={state.txInfo.txPayload}
+            params={state.txConfig.params}
+            partialFee={state.txInfo.partialFee}
             onCancel={reset}
             isExternalAccount={state.isExternalAccount}
-            onConfirm={() => {
+            onConfirm={async () => {
               if (state.isExternalAccount) {
-                dispatch({type: 'SHOW_QR_CODE_TX_PAYLOAD_VIEW', payload: {txPayload: state.txPayload}});
+                dispatch({
+                  type: 'SHOW_QR_CODE_TX_PAYLOAD_VIEW',
+                  payload: {address: state.address, txConfig: state.txConfig, txInfo: state.txInfo},
+                });
               } else {
                 dispatch({type: 'SHOW_SUBMITTING_VIEW'});
-                signTransactionRef.current?.(state.signerResult);
+                const signable = u8aToHex(state.txInfo.signablePayload);
+                const signed = await sign({message: signable, credentials: state.credentials});
+                sendTransaction({
+                  address: state.credentials.address,
+                  txConfig: state.txConfig,
+                  txPayload: state.txInfo.txPayload,
+                  signature: signed,
+                });
               }
             }}
           />
@@ -223,9 +159,14 @@ export function TxProvider({children}: TxProviderProps): React.ReactElement {
       case 'qr_code_tx_payload_view':
         return (
           <PayloadQrCodeView
-            payload={state.txPayload}
+            payload={state.txInfo.txPayload}
             onCancel={reset}
-            onConfirm={() => dispatch({type: 'SHOW_SCAN_SIGNATURE_VIEW'})}
+            onConfirm={() =>
+              dispatch({
+                type: 'SHOW_SCAN_SIGNATURE_VIEW',
+                payload: {address: state.address, txConfig: state.txConfig, txInfo: state.txInfo},
+              })
+            }
           />
         );
 
@@ -237,9 +178,12 @@ export function TxProvider({children}: TxProviderProps): React.ReactElement {
             <QRCodeScanner
               onRead={(data) => {
                 dispatch({type: 'SHOW_SUBMITTING_VIEW'});
-                signTransactionRef.current?.({
-                  id: id++,
-                  signature: `0x${data.data}`,
+                const signature: HexString = `0x${data.data}`;
+                sendTransaction({
+                  address: state.address,
+                  txConfig: state.txConfig,
+                  txPayload: state.txInfo.txPayload,
+                  signature,
                 });
               }}
               showMarker
@@ -291,14 +235,13 @@ export function TxProvider({children}: TxProviderProps): React.ReactElement {
       default:
         return null;
     }
-  }, [state, reset]);
+  }, [state, reset, sign, sendTransaction]);
 
-  const contextValue: TxContextValueType = useMemo(() => ({start}), [start]);
+  const contextValue: TxContextValueType = useMemo(() => ({startTx}), [startTx]);
 
   const onCloseBottomSheet = useCallback(() => {
     dispatch({type: 'RESET'});
-    signTransactionRef.current = undefined;
-  }, [signTransactionRef]);
+  }, []);
 
   return (
     <TxContext.Provider value={contextValue}>
@@ -308,7 +251,7 @@ export function TxProvider({children}: TxProviderProps): React.ReactElement {
   );
 }
 
-export function useTx() {
+export function useStartTx() {
   const context = useContext(TxContext);
 
   if (!context) {
@@ -360,28 +303,29 @@ const styles = StyleSheet.create({
 
 type State =
   | {view: 'initial_view' | 'submitting_view' | 'success_view'}
-  | {view: 'authenticate_view'; txPayload: SignerPayloadJSON; address: string; seed?: string}
+  | {view: 'authenticate_view'; address: string; txConfig: TxConfig; txInfo: TxInfo}
   | {
       view: 'tx_preview';
-      txPayload: SignerPayloadJSON;
-      params: unknown[];
-      title: string;
-      description: string;
-      partialFee: number;
+      txConfig: TxConfig;
+      txInfo: TxInfo;
+      address: string;
       isExternalAccount: true;
     }
   | {
       view: 'tx_preview';
-      txPayload: SignerPayloadJSON;
-      params: unknown[];
-      title: string;
-      description: string;
-      partialFee: number;
+      txConfig: TxConfig;
+      txInfo: TxInfo;
+      credentials: SignCredentials;
       isExternalAccount: false;
-      signerResult: SignerResult;
     }
-  | {view: 'qr_code_tx_payload_view'; txPayload: SignerPayloadJSON}
-  | {view: 'scan_signature_view'}
+  | {view: 'qr_code_tx_payload_view'; address: string; txConfig: TxConfig; txInfo: TxInfo}
+  | {
+      view: 'scan_signature_view';
+      address: string;
+      txConfig: TxConfig;
+      txInfo: TxInfo;
+      isExternalAccount: true;
+    }
   | {view: 'error_view'; error: string}
   | {view: 'warning_view'; warning: string};
 
@@ -392,40 +336,38 @@ type Action =
   | {
       type: 'SHOW_AUTHENTICATE_VIEW';
       payload: {
-        txPayload: SignerPayloadJSON;
         address: string;
-        seed?: string;
+        txConfig: TxConfig;
+        txInfo: TxInfo;
       };
     }
   | {
       type: 'SHOW_TX_PREVIEW';
       payload:
         | {
-            txPayload: SignerPayloadJSON;
-            partialFee: number;
-            params: unknown[];
-            title: string;
-            description: string;
+            txInfo: TxInfo;
+            txConfig: TxConfig;
+            address: string;
             isExternalAccount: true;
           }
         | {
-            txPayload: SignerPayloadJSON;
-            partialFee: number;
-            params: unknown[];
-            title: string;
-            description: string;
+            txConfig: TxConfig;
+            txInfo: TxInfo;
+            credentials: SignCredentials;
             isExternalAccount: false;
-            signerResult: SignerResult;
           };
     }
   | {
       type: 'SHOW_QR_CODE_TX_PAYLOAD_VIEW';
       payload: {
-        txPayload: SignerPayloadJSON;
+        address: string;
+        txConfig: TxConfig;
+        txInfo: TxInfo;
       };
     }
   | {
       type: 'SHOW_SCAN_SIGNATURE_VIEW';
+      payload: {address: string; txConfig: TxConfig; txInfo: TxInfo};
     }
   | {type: 'SHOW_SUBMITTING_VIEW'}
   | {type: 'SHOW_ERROR'; payload: string}
@@ -438,21 +380,16 @@ function reducer(state: State, action: Action): State {
       return initialState;
 
     case 'SHOW_AUTHENTICATE_VIEW':
-      return {
-        view: 'authenticate_view',
-        txPayload: action.payload.txPayload,
-        seed: action.payload.seed,
-        address: action.payload.address,
-      };
+      return {view: 'authenticate_view', ...action.payload};
 
     case 'SHOW_TX_PREVIEW':
       return {view: 'tx_preview', ...action.payload};
 
     case 'SHOW_QR_CODE_TX_PAYLOAD_VIEW':
-      return {view: 'qr_code_tx_payload_view', txPayload: action.payload.txPayload};
+      return {view: 'qr_code_tx_payload_view', ...action.payload};
 
     case 'SHOW_SCAN_SIGNATURE_VIEW':
-      return {view: 'scan_signature_view'};
+      return {view: 'scan_signature_view', ...action.payload, isExternalAccount: true};
 
     case 'SHOW_SUBMITTING_VIEW':
       return {view: 'submitting_view'};
