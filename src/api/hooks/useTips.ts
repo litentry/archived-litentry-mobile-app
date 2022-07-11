abb860824b3a8ff5f19a1ab1ccf7f2bf4287df31
Ishrak Imam <ishrak@litentry.com>
2022-07-11 16:14:02 +0200

    Updating tips query

--- src/api/hooks/useTips.ts
+++ src/api/hooks/useTips.ts
@@ -1,11 +1,21 @@
 import {gql, NetworkStatus, useQuery} from '@apollo/client';
-import {SubstrateChainTip, SubstrateChainTipStatus} from 'src/generated/litentryGraphQLTypes';
+import {
+  SubstrateChainTip,
+  SubstrateChainTipStatus,
+  SubstrateChainTipsOrderByInput,
+} from 'src/generated/litentryGraphQLTypes';
 
 export type Tip = SubstrateChainTip;
+export const TipsOrderByInput = SubstrateChainTipsOrderByInput;
 
 export const TIPS_QUERY = gql`
-  query getTips($status: [SubstrateChainTipStatus!]) {
-    substrateChainTips(status: $status) {
+  query getTips(
+    $status: [SubstrateChainTipStatus!]
+    $limit: Int
+    $offset: Int
+    $orderBy: SubstrateChainTipsOrderByInput
+  ) {
+    substrateChainTips(status: $status, limit: $limit, offset: $offset, orderBy: $orderBy) {
       id
       createdAt
       who {
@@ -20,15 +30,29 @@ export const TIPS_QUERY = gql`
 
 type TIP_STATUS = keyof typeof SubstrateChainTipStatus;
 
-export function useTips(status: TIP_STATUS[]) {
+type TipsQueryParams = {
+  status?: TIP_STATUS[];
+  limit?: number;
+  offset?: number;
+  orderBy?: typeof TipsOrderByInput[keyof typeof TipsOrderByInput];
+};
+
+export function useTips({
+  status = ['Closed', 'Opened', 'Retracted', 'Slashed'],
+  limit = 20,
+  offset = 0,
+  orderBy = TipsOrderByInput.CreatedAtDesc,
+}: TipsQueryParams) {
   const tipStatus = status.map((st) => SubstrateChainTipStatus[st]);
   const {data, networkStatus, ...rest} = useQuery<{substrateChainTips: SubstrateChainTip[]}>(TIPS_QUERY, {
-    variables: {status: tipStatus},
+    variables: {status: tipStatus, limit, offset, orderBy},
+    notifyOnNetworkStatusChange: true,
   });
 
   return {
     data: data?.substrateChainTips,
     refetching: networkStatus === NetworkStatus.refetch,
+    fetchingMore: networkStatus === NetworkStatus.fetchMore,
     ...rest,
   };
 }
