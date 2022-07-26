8d49c9032d61bf5928ee5093adf1912d230701d5
Ishrak Imam <ishrak@litentry.com>
2022-07-26 22:32:20 +0200

    Adding custom merge function for substrateChainDemocracyProposals

--- src/context/LitentryApiContext.tsx
+++ src/context/LitentryApiContext.tsx
@@ -50,6 +50,17 @@ export function LitentryApiClientProvider({children}: {children: React.ReactNode
                   return merged;
                 },
               },
+              substrateChainDemocracyProposals: {
+                keyArgs: [],
+                merge(existing, incoming, {args}) {
+                  const offset = args ? args.offset : 0;
+                  const merged = existing ? existing.slice(0) : [];
+                  for (let i = 0; i < incoming.length; ++i) {
+                    merged[offset + i] = incoming[i];
+                  }
+                  return merged;
+                },
+              },
             },
           },
         },
